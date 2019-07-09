70ab1bc55089d9c37b11989c88d016b9cfd14fa7
Yashodhan Pise <yashodhan.pise@seagate.com>
2019-07-09 16:30:49 +0530

    EOS-685: First draft

---
+++ srv/_modules/eosconfig.py
@@ -0,0 +1,18 @@
+
+
+def _read_config_file(config_filename: str) -> dict:
+  pass
+
+def _read_pillar_file(ref_component_pillar: str) -> dict:
+  pass
+
+def update(name: str, ref_pillar: str, backup: bool=True, file_type: str='yaml') -> bool:
+  """Update component config file.
+
+  Args :
+    name: Destination path of component config file to be updated
+    ref_pillar: Reference section from pillar data for a component to be updated
+    backup: Backup config file before modification as bool (Default: True)
+    backup: Backup config file before modification as bool (Default: True)
+  """
+  pass
