cd0ee69702e8d4fdb42b4c9f1f1950a5d3e29e24
spati <sradhanand.pati@seagate.com>
2019-08-01 04:01:01 +0000

    EOS-688 Validate pillar data 	modified:   utils/components/cluster_cfg.py 	new file:   utils/components/cluster_schema.yaml 	modified:   utils/components/eoscore_cfg.py 	new file:   utils/components/eoscore_schema.yaml 	modified:   utils/components/haproxy_cfg.py 	new file:   utils/components/haproxy_schema.yaml 	modified:   utils/components/release_cfg.py 	new file:   utils/components/release_schema.yaml 	modified:   utils/components/s3client_cfg.py 	new file:   utils/components/s3client_schema.yaml 	modified:   utils/components/s3server_cfg.py 	new file:   utils/components/s3server_schema.yaml 	modified:   utils/components/sspl_cfg.py 	new file:   utils/components/sspl_schema.yaml

--- utils/components/haproxy_cfg.py
+++ utils/components/haproxy_cfg.py
@@ -2,7 +2,7 @@
 import json
 import os.path
 import yaml
-
+import re
 from argparse import ArgumentParser, Namespace
 
 from .base_cfg import BaseCfg
@@ -11,10 +11,16 @@ from .base_cfg import BaseCfg
 class HAProxyCfg(BaseCfg):
     __options = {}
     __cfg_path = ""
+    __schema = {}
 
 
     def __init__(self, cfg_path: str=None, arg_parser: ArgumentParser=None):
 
+        self.__schema_path = os.path.join(os.path.join(
+                    os.path.dirname(os.path.realpath(__file__)),
+                ),
+                "haproxy_schema.yaml"
+            )
         if cfg_path:
             self.__cfg_path = cfg_path
         else:
@@ -57,8 +63,14 @@ class HAProxyCfg(BaseCfg):
 
         with open(self.__cfg_path, 'r') as fd:
             self.__options = yaml.load(fd, Loader=yaml.FullLoader)
+        with open(self.__schema_path, 'r') as fd:
+            self.__schema = yaml.safe_load(fd)
         # print(json.dumps(self.__options, indent = 4))
         # TODO validations for configs.
+        try:
+            self.validate(self.__schema, self.__options)
+        except Exception as e:
+            print("Validation Failed for {} : {}".format("haproxy.sls",str(e)))
 
 
     def process_inputs(self, program_args: Namespace) -> bool:
@@ -145,4 +157,45 @@ class HAProxyCfg(BaseCfg):
 
 
     def validate(self, schema_dict: dict, pillar_dict: dict) -> bool:
-        pass
+        """
+        This function validates the pillar dict with the schema dict
+
+        Parameters :
+        -----------
+        schema_dict : dict
+        The schema dict representation of a pillar data
+        pillar_dict : dict
+        The data dict of a pillar data
+
+        Returns:
+        -------
+        bool
+        True if pillar dict is validated with schema dict
+        """
+        for key, value in schema_dict.items():
+            if key in pillar_dict:
+                if pillar_dict[key].__class__.__name__ == schema_dict[key]["type"]:
+                    if pillar_dict[key] is None and "required" in schema_dict[key] and schema_dict[key]["required"]:
+                        if "default" in schema_dict[key]:
+                            pillar_dict[key] = schema_dict[key]["default"]
+                        else:
+                            raise Exception(
+                                "Default value missing for a required key {}".format(key))
+                    if isinstance(pillar_dict[key], dict):
+                        self.validate(schema_dict[key]["schema"], pillar_dict[key])
+                    elif "format" in schema_dict[key] and schema_dict[key]["format"] == "ip-address":
+                        match_exp = re.match(
+                            r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", pillar_dict[key])
+                        if not (bool(match_exp) and all(
+                                map(lambda n: 0 <= int(n) <= 255, match_exp.groups()))):
+                            raise Exception(
+                                "ip-address is of invalid format for key {}".format(key))
+                    if "option" in schema_dict[key] and str(pillar_dict[key]).lower() not in schema_dict[key]["option"].lower().split("/"):
+                        raise Exception("Unsupported value for key {}. Value should be one of {} ".format(key,schema_dict[key]["option"].split("/")))
+                else:
+                    raise Exception("Invalid Input for key {}. Expected {} instead of {}".format(
+                        key, schema_dict[key]["type"], pillar_dict[key].__class__.__name__))
+            else:
+                raise Exception("Key not found {}".format(key))
+        return True
+
--- utils/components/s3client_cfg.py
+++ utils/components/s3client_cfg.py
@@ -2,7 +2,7 @@
 import json
 import os.path
 import yaml
-
+import re
 from argparse import ArgumentParser, Namespace
 
 from .base_cfg import BaseCfg
@@ -11,10 +11,16 @@ from .base_cfg import BaseCfg
 class S3ClientCfg(BaseCfg):
     __options = {}
     __cfg_path = ""
+    __schema = {}
 
 
     def __init__(self, cfg_path: str=None, arg_parser: ArgumentParser=None):
 
+        self.__schema_path = os.path.join(os.path.join(
+                    os.path.dirname(os.path.realpath(__file__)),
+                ),
+                "s3client_schema.yaml"
+            )
         if cfg_path:
             self.__cfg_path = cfg_path
         else:
@@ -61,6 +67,12 @@ class S3ClientCfg(BaseCfg):
             self.__options = yaml.load(fd, Loader=yaml.FullLoader)
         # print(json.dumps(self.__options, indent = 4))
         # TODO validations for configs.
+        with open(self.__schema_path, 'r') as fd:
+            self.__schema = yaml.safe_load(fd)
+        try:
+            self.validate(self.__schema, self.__options)
+        except Exception as e:
+            print("Validation Failed for {} : {}".format("s3client.sls",str(e)))
 
 
     def process_inputs(self, program_args: Namespace) -> bool:
@@ -160,4 +172,45 @@ class S3ClientCfg(BaseCfg):
 
 
     def validate(self, schema_dict: dict, pillar_dict: dict) -> bool:
-        pass
+        """
+        This function validates the pillar dict with the schema dict
+
+        Parameters :
+        -----------
+        schema_dict : dict
+        The schema dict representation of a pillar data
+        pillar_dict : dict
+        The data dict of a pillar data
+
+        Returns:
+        -------
+        bool
+        True if pillar dict is validated with schema dict
+        """
+        for key, value in schema_dict.items():
+            if key in pillar_dict:
+                if pillar_dict[key].__class__.__name__ == schema_dict[key]["type"]:
+                    if pillar_dict[key] is None and "required" in schema_dict[key] and schema_dict[key]["required"]:
+                        if "default" in schema_dict[key]:
+                            pillar_dict[key] = schema_dict[key]["default"]
+                        else:
+                            raise Exception(
+                                "Default value missing for a required key {}".format(key))
+                    if isinstance(pillar_dict[key], dict):
+                        self.validate(schema_dict[key]["schema"], pillar_dict[key])
+                    elif "format" in schema_dict[key] and schema_dict[key]["format"] == "ip-address":
+                        match_exp = re.match(
+                            r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", pillar_dict[key])
+                        if not (bool(match_exp) and all(
+                                map(lambda n: 0 <= int(n) <= 255, match_exp.groups()))):
+                            raise Exception(
+                                "ip-address is of invalid format for key {}".format(key))
+                    if "option" in schema_dict[key] and str(pillar_dict[key]).lower() not in schema_dict[key]["option"].lower().split("/"):
+                        raise Exception("Unsupported value for key {}. Value should be one of {} ".format(key,schema_dict[key]["option"].split("/"))) 
+                else:
+                    raise Exception("Invalid Input for key {}. Expected {} instead of {}".format(
+                        key, schema_dict[key]["type"], pillar_dict[key].__class__.__name__))
+            else:
+                raise Exception("Key not found {}".format(key))
+        return True
+
