88292ed39b1f434c086ea4ada1feae2058bc904f
Yashodhan Pise <yashodhan.pise@seagate.com>
2019-08-23 05:42:15 +0000

    EOS-1303: Provisioner: Updating S3Server config interrupts hctl bootstrap

--- srv/_modules/s3server.py
+++ srv/_modules/s3server.py
@@ -7,40 +7,38 @@ from shutil import copyfile
 
 # How to test:
 # $ salt-call saltutil.clear_cache
-# $ salt-call saltutil.sync_modules && salt-call s3server.conf_update "/opt/seagate/s3/conf/s3config.yaml" s3server
+# $ salt-call saltutil.sync_modules && salt-call s3server.conf_update "/opt/seagate/ees-prvsnr/srv/_modules/files/samples/s3config.yaml" s3server
 
 # def _read_pillar(ref_component_pillar: str) -> dict:
 def _read_pillar(ref_component_pillar):
   return __pillar__[ref_component_pillar]
 
 
-# def update(config_file: str, ref_pillar: str, type: str=None, backup: bool=True) -> bool:
+# def update(name: str, ref_pillar: str, type: str=None, backup: bool=True) -> bool:
 def conf_update(
-    config_file="/opt/seagate/s3/conf/s3config.yaml",
+    name="/opt/seagate/s3/conf/s3config.yaml",
     ref_pillar="s3server",
-    type=None,
     backup=True
   ):
   """Update component config file.
 
   Args :
-    config_file: Destination path of component config file to be updated
+    name: Destination path of component config file to be updated
     ref_pillar: Reference section from pillar data for a component to be updated
-    type: Type of config file YAML/INI
     backup: Backup config file before modification as bool (Default: True)
   """
   pillar_dict = _read_pillar(ref_pillar)
   
-  if not os.path.exists(config_file):
+  if not os.path.exists(name):
     print("[ERROR   ] Possible error with s3server installation.")
     raise FileNotFoundError(
       errno.ENOENT,
       os.strerror(errno.ENOENT),
-      config_file
+      name
     )
 
   config_dict = {}
-  with open(config_file, "r") as fd:
+  with open(name, "r") as fd:
     try:
       config_dict = yaml.safe_load(fd)
     except yaml.YAMLError as yerr:
@@ -51,13 +49,21 @@ def conf_update(
         return False
 
   if backup:
-    copyfile(config_file, config_file + '.bak')
+    copyfile(name, name + '.bak')
 
   config_dict = _update_dict(config_dict, pillar_dict)
-  yaml.add_representer(_BlockSeqList, _blockseqlist_rep)
+  yaml.add_representer(list, _blockseqlist_rep)
+  yaml.add_representer(type(None), _represent_none)
 
-  with open(config_file, 'w') as fd:
-    yaml.dump(config_dict, fd)
+  with open(name, 'w') as fd:
+    yaml.dump(
+        config_dict,
+        stream=fd,
+        default_flow_style = False,
+        canonical=False,
+        width=1,
+        indent=4
+    )
 
   return True
 
@@ -67,16 +73,14 @@ def _update_dict(config_dict, pillar_dict):
     if key in pillar_dict:
       if isinstance(config_dict[key], dict):
         _update_dict(config_dict[key], pillar_dict[key])
-      elif pillar_dict[key].__class__.__name__ == "list":
-        config_dict[key] = _BlockSeqList(pillar_dict[key])
       else:
         config_dict[key] = pillar_dict[key]
   
   return config_dict
 
 
-class _BlockSeqList( list ): pass
-
-
 def _blockseqlist_rep(dumper, data):
   return dumper.represent_sequence(u'tag:yaml.org,2002:seq', data, flow_style = True)
+
+def _represent_none(dumper, _):
+  return dumper.represent_scalar('tag:yaml.org,2002:null', '~')
