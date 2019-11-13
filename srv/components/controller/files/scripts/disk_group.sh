b3eec5abe591081f15bf6fbaabfaec7d5314c44f
Pritam Bhavsar <pritam.bhavsar@seagate.com>
2019-11-13 20:03:39 +0530

    EOS-2500 Provisioner: Controller CLI automation
    Adding patches for:
    1: EOS 2517:- Cleanup the existing provisioning
    2: EOS 2518:- Parse xml output received from CLI

--- srv/components/controller/files/scripts/disk_group.sh
+++
@@ -1,9 +0,0 @@
-add_disk_group()
-{
-    _dg=$1
-    _disks_range=$2
-    _pool=$3
-    _cmd="add disk-group type virtual disks $disks_range level adapt pool $_pool $_dg"
-    echo "Creating disk group $_dg with $_disks_range disks"
-    cmd_run $_cmd
-}
--- srv/components/controller/files/scripts/license.sh
+++ srv/components/controller/files/scripts/license.sh
@@ -1,5 +1,27 @@
 check_license()
 {
-    #TODO: Check if the virtualizztion license is enabled
+    tmp_file="/tmp/license"
+    # objects name in the xml
+    object_basetype="license"
+    # list of properties of the object basetype to get their values from the
+    # xml e.g. license for virtualization & vds
+    property_list=("virtualization" "vds")
+
+    # run command to get the available ports
     cmd_run 'show license'
+    # parse xml to get required values of properties
+    parse_xml $xml_doc $object_basetype "${property_list[@]}" > $tmp_file
+
+    # Get the status of virtualization license
+    virtualization_license=`cat $tmp_file | awk '{ print $1 }'`
+    vds_license=`cat $tmp_file | awk '{ print $2 }'`
+    [[ $virtualization_license = "Enabled" ]] || {
+        echo "Virtualization license is not installed."
+        exit 1
+    }
+    # Get the status of vds license
+    [[ $vds_license = "Enabled" ]] || {
+        echo "VDS license is not installed."
+        exit 1
+    }
 }
