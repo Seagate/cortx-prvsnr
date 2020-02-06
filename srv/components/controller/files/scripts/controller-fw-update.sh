0fab9707c27e34f44f6ecada9cd0b19202a9cad1
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2020-02-06 16:39:00 +0530

    EOS-5317 Added new logic for parse

--- srv/components/controller/files/scripts/controller-fw-update.sh
+++ srv/components/controller/files/scripts/controller-fw-update.sh
@@ -13,7 +13,7 @@ usage()
     cat <<USAGE
     Usage:
     $0 -c1 <controller-A> -c2 <controller-B> -u <username> -p '<password>'\
-     <firmware_bundle_file>
+    <firmware_bundle_file>
     Where,
         controller-A         :- hostname or ip address of controller-A
         controller-B         :- hostname or ip address of controller-B
@@ -26,26 +26,22 @@ USAGE
 
 parse_opts()
 {
-    unset $cntrl_a
-    unset $cntrl_b
     unset $user
     unset $pass
     echo "parse_opts(): No. of arguments:$#, arguments=$@">> $fw_logfile
-    [ $# -lt 8 ] && {
+    [ $# -lt 4 ] && {
         echo "Insufficient arguments provided" && usage && exit 1
     }
-    while getopts 'c1:c2:u:p:' opt 
+    while getopts 'u:p:' opt 
     do
         case $opt in
-            c1) cntrl_a="$OPTARG";;
-            c2) cntrl_b="$OPTARG";;
             u) user="$OPTARG";;
             p) pass="$OPTARG";;
             ?) echo "Unrecognized option '$OPTARG'"; usage; exit 1;;
             *) usage; exit 1;;
         esac
     done
-    [ -z "$cntrl_a" -o -z "$cntrl_b" -o -z "$user" -o -z "$pass" ] && {
+    [ -z "$user" -o -z "$pass" ] && {
         echo "Error: proper input not provided" && usage && exit 1
     }    
 }
@@ -55,9 +51,14 @@ parse_args()
     [ $# -lt 9 ] && {
         echo "Error: Insufficient arguments provided" && usage && exit 1
     }
-    parse_opts $1 $2 $3 $4 $5 $6 $7 $8
+    [[ $1 != "-c1" -a $3 != "-c2" ]] && {
+        echo "Error: proper input not provided" && usage && exit 1
+    }
+    cntrl_a=$2
+    cntrl_b=$4
+    parse_opts $5 $6 $7 $8
     fw_bundle=$9
-    [ -z "$fw_bundle" ] && {
+    [ -z $cntrl_a -o -z $cntrl_b -o -z "$fw_bundle" ] && {
         echo "Error: proper input not provided" && usage && exit 1
     }
 }
