10a76692f24f2578e758fa958af0c9b5303312d0
Yashodhan Pise <yashodhan.pise@seagate.com>
2020-04-05 17:36:21 +0000

    EOS-6379: Password encryption eos config

---
+++ srv/_modules/lyveutil.py
@@ -0,0 +1,22 @@
+# How to test:
+# salt-call saltutil.clear_cache && salt-call saltutil.sync_modules
+# salt-call lyveutil.decrypt "secret" "component"
+
+from salt import client
+
+from eos.utils.security.cipher import Cipher, CipherInvalidToken
+
+def decrypt(secret, component):
+    """ Decrypt secret.
+
+    Args:
+      secret: Secret to be decrypted.
+    """
+    retval = None
+    cluster_id = __grains__['cluster_id']
+    cipher_key = Cipher.generate_key(cluster_id, component)
+
+    if secret:
+        retval = (Cipher.decrypt(cipher_key, secret.encode("utf-8"))).decode("utf-8")
+      
+    return retval
