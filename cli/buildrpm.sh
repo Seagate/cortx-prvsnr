e76f2e2d0023d9ce35aac0a49f6d01ad40a43232
Yashodhan Pise <yashodhan.pise@seagate.com>
2020-04-08 12:45:21 +0530

    Fixed build script for eos-prvsnr-cli

--- cli/buildrpm.sh
+++ cli/buildrpm.sh
@@ -62,6 +62,7 @@ pushd ~/rpmbuild/SOURCES/
     mkdir -p ${DEST_DIR}/files/.ssh/
     mkdir -p ${DEST_DIR}/files/syslogconfig/
     cp -pr ${BASEDIR}/src/* ${DEST_DIR}/cli
+    cp -pr ${BASEDIR}/utils ${DEST_DIR}/cli
     cp -pr ${BASEDIR}/../files/etc/salt/* ${DEST_DIR}/files/etc/salt
     # cp -pr ${BASEDIR}/../files/etc/sysconfig/network-scripts/ifcfg-* ${DEST_DIR}/files/etc/sysconfig/network-scripts/
     cp -p ${BASEDIR}/../files/etc/modprobe.d/bonding.conf ${DEST_DIR}/files/etc/modprobe.d/bonding.conf
