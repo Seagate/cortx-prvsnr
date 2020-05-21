c4060ea16d4bdf1535c73909b7429a7af0c4294e
Alexander Nogikh <alexander.nogikh@seagate.com>
2020-05-21 07:16:17 -0600

    Enhanced the script: refactoring, added partition matching

--- files/scripts/setup/raid_conversion.py
+++ files/scripts/setup/raid_conversion.py
@@ -42,6 +42,9 @@ def ensure(condition, comment):
 def info(comment):
   print(comment)
 
+def debug(comment):
+  print(comment)
+
 ### Helpers
 
 def check_root() -> bool:
@@ -52,32 +55,65 @@ def strip_disk(disk_name) -> str:
     return re.sub('\d*$', '', disk_name)
   return disk_name
 
+def get_partition(disk_name) -> str:
+  if disk_name.startswith("/dev/sd"):
+    return re.search(r'([0-9]+)$', disk_name).group(1)
+  return None
 
 ### Queries
 
-def query_mdadm(device_name):
-  result_size = sh(f"mdadm -Q --detail {device_name} | grep 'Array Size'")
-  if result_size.stderr:
-    return None
+class RaidArray:
+  name: str
 
-  result_state = sh(f"mdadm -Q --detail {device_name} | grep 'State'")
-  result_devices = sh_lines(f"mdadm -Q --detail {device_name} |  grep -A 10 'RaidDevice' | tail -n +2")
+  def __init__(self, name):
+    self.name = name
 
-  devices = []
-  for device_line in result_devices.stdout:
-    columns = re.split(r'\s{2,}', device_line)
-    devices.append({
-      "raid_device": columns[4],
-      "state": columns[5],
-      "device": columns[6]
-    })
+  @property
+  def devices(self):
+    result_devices = sh_lines(f"mdadm -Q --detail {self.name} |  grep -A 10 'RaidDevice' | tail -n +2")
+    devices = []
+    for device_line in result_devices.stdout:
+      columns = re.split(r'\s{2,}', device_line)
+      devices.append({
+        "raid_device": columns[4],
+        "state": columns[5],
+        "device": columns[6]
+      })
+    return devices
 
-  return {
-    "array_size": re.search(r':\s+([0-9]+)\s', result_size.stdout).group(1),
-    "state": re.search(r':\s+([^\s]+)\s', result_state.stdout).group(1),
-    "devices": devices
-  }
-  
+  @property
+  def state(self):
+    result_state = sh(f"mdadm -Q --detail {self.name} | grep 'State'")
+    if result_state.stderr:
+      return None
+
+    return re.search(r':\s+([^\s]+)\s', result_state.stdout).group(1)
+
+  @property
+  def array_size(self):
+    result_size = sh(f"mdadm -Q --detail {self.name} | grep 'Array Size'")
+    if result_size.stderr:
+      return None
+    return re.search(r':\s+([0-9]+)\s', result_size.stdout).group(1)
+
+  def ensure_valid(self, required_device_no):
+    state = self.state
+    ensure(state, f"{self.name} must be present")
+    ensure(state in ['active', 'clean'], f"{self.name} must be clean or active")
+    ensure(len(self.devices) == required_device_no, f"{self.name} must have exactly {required_device_no} raid devices")
+
+  def filter_devices(self, device_name):
+    return [x["device"] for x in self.devices if strip_disk(x["device"]) == strip_disk(device_name)]
+
+  def fail_device(self, device_name):
+    ensure(device_name in self.filter_devices(device_name), f"{device_name} must be included in {self.name}")
+    result = sh(f"mdadm {self.name} --fail {device_name}")
+    return not result.stderr
+
+  def remove_device(self, device_name):
+    result = sh(f"mdadm {self.name} --remove {device_name}")
+    return not result.stderr
+
 def query_pvs_devices():
   raw_result = sh_lines(f"pvdisplay | grep 'PV Name'")
   if raw_result.stderr:
@@ -87,6 +123,7 @@ def query_pvs_devices():
   for line in raw_result.stdout:
     result.append(re.split(r'\s{2,}', line)[2])
   return result
+
 
 def query_mounts():
   raw_result = sh_lines("cat /proc/mounts")
@@ -99,36 +136,108 @@ def query_mounts():
     result.append({ "device": columns[0], "mount_point": columns[1] })
   return result
 
-def query_partitions(disk):
-  raw_result = sh_lines(f"parted --machine -s {disk} unit B print | tail -n +3")
-  result = []
-  for line in raw_result.stdout:
-    columns = re.split(r':', re.sub('[\;]?$', '', line))
-    result.append({
-      "number": columns[0],
-      "size": re.sub('[^\d]*$', '', columns[3]),
-      "file_system": columns[4],
-      "name": columns[5],
-      "flags": columns[6]
-    })
-  return result
-
+  
 def query_multipath_devices():
   return [ "/dev/mapper/" + x for x in sh_lines("multipath -ll -v 1").stdout]
 
-### Script
+
+class PartitionList:
+  partitions = []
+  def __init__(self, partitions):
+    self.partitions = partitions
+
+  def filter_by_number(self, ids):
+    return PartitionList([x for x in self.partitions if x["number"] in ids])
+
+  def filter_raid(self):
+    return PartitionList([x for x in self.partitions if "raid" in x["flags"]])
+
+  def filter_nonraid(self):
+    return PartitionList([x for x in self.partitions if "raid" not in x["flags"]])
+
+  def match_to(self, right_partitions) -> dict:
+    debug(f"Matching {self.partitions} with {right_partitions.partitions}")
+
+    right_tmp = right_partitions.partitions.copy()
+    response = {}
+    for left_partition in self.partitions:
+      match = next((x for x in right_tmp if x["size"] == left_partition["size"]), None)
+      if not match:
+        return None
+      response[left_partition["number"]] = match["number"]
+      right_tmp.remove(match)
+    return response
+
+  @staticmethod
+  def query_partitions(disk):
+    raw_result = sh_lines(f"parted --machine -s {disk} unit B print | tail -n +3")
+    result = []
+    for line in raw_result.stdout:
+      columns = re.split(r':', re.sub('[\;]?$', '', line))
+      result.append({
+        "number": columns[0],
+        "size": re.sub('[^\d]*$', '', columns[3]),
+        "file_system": columns[4],
+        "name": columns[5],
+        "flags": columns[6]
+      })
+    return PartitionList(result)
+
+
+### Script routines
+
+def resolve_partitions(primary_disk, enclosure_disk):
+  """
+  Ideally this function must also take care of re-partitioning the enclosure disk
+  """
+
+  primary_disk_partitions = PartitionList.query_partitions(primary_disk)
+  enclosure_partitions = PartitionList.query_partitions(enclosure_disk)
+
+  efi_partition = get_partition(efi_disk)
+  boot_partition = get_partition(md0.filter_devices(primary_disk)[0])
+  lvm_partition = get_partition(md1.filter_devices(primary_disk)[0])
+
+  info(f"/boot/efi partition on {primary_disk}: {efi_partition}")
+  info(f"/boot partition on {primary_disk}: {boot_partition}")
+  info(f"LVM partition on {primary_disk}: {lvm_partition}")
+
+  efi_match = primary_disk_partitions.filter_by_number([efi_partition]) \
+    .match_to(enclosure_partitions.filter_nonraid())
+  ensure(efi_match, "Cannot match /boot/efi partition")
+
+  nonefi_match = primary_disk_partitions.filter_by_number([boot_partition, lvm_partition]) \
+    .match_to(enclosure_partitions.filter_raid())
+  ensure(nonefi_match, "Cannot match /boot and LVM partitions")
+
+  encl_efi_partition = efi_match[efi_partition]
+  encl_boot_partition = nonefi_match[boot_partition]
+  encl_lvm_partition = nonefi_match[lvm_partition]
+
+  info(f"/boot/efi will be placed on partition {encl_efi_partition} of {enclosure_disk}")
+  info(f"/boot will be placed on partition {encl_boot_partition} of {enclosure_disk}")
+  info(f"LVM will be placed on partition {encl_lvm_partition} of {enclosure_disk}")
+
+  return {
+    "efi": [efi_partition, encl_efi_partition],
+    "boot": [boot_partition, encl_boot_partition],
+    "lvm": [lvm_partition, encl_lvm_partition]
+  }
+
+def move_boot_efi(primary_id, enclosure_id):
+  pass
+  
+def move_raid_partition(raid_array, primary_id, enclosure_id):
+  pass
+
+### Script 
 
 ensure(check_root(), "Script must be run by a root user")
-md0 = query_mdadm("/dev/md0")
-md1 = query_mdadm("/dev/md1")
+md0 = RaidArray("/dev/md0")
+md1 = RaidArray("/dev/md1")
 
-ensure(md0, "/dev/md0 must be present")
-ensure(md0["state"] in ['active', 'clean'], "/dev/md0 must be clean or active")
-ensure(len(md0["devices"]) == 2, "/dev/md0 must have exactly two raid devices")
-
-ensure(md1, "/dev/md1 must be present")
-ensure(md1["state"] in ['active', 'clean'], "/dev/md1 must be clean or active")
-ensure(len(md1["devices"]) == 2, "/dev/md1 must have exactly two raid devices")
+md0.ensure_valid(2)
+md1.ensure_valid(2)
 
 pvs_devices = query_pvs_devices()
 ensure('/dev/md1' in pvs_devices, "There must be a physical volume on /dev/md1")
@@ -152,16 +261,20 @@ secondary_disk = strip_disk(efi2_disk)
 info(f"Primary HDD: {primary_disk}")
 info(f"Secondary HDD: {secondary_disk}")
 
-ensure(any(strip_disk(x["device"]) == primary_disk for x in md0["devices"]), "Disk with /boot/efi must be included in /dev/md0")
-ensure(any(strip_disk(x["device"]) == secondary_disk for x in md0["devices"]), "Disk with /boot/efi2 must be included in /dev/md0")
-ensure(any(strip_disk(x["device"]) == primary_disk for x in md1["devices"]), "Disk with /boot/efi must be included in /dev/md1")
-ensure(any(strip_disk(x["device"]) == secondary_disk for x in md1["devices"]), "Disk with /boot/efi2 must be included in /dev/md1")
+ensure(md0.filter_devices(primary_disk), "Disk with /boot/efi must be included in /dev/md0")
+#ensure(md0.filter_devices(secondary_disk), "Disk with /boot/efi2 must be included in /dev/md0")
+ensure(md1.filter_devices(primary_disk), "Disk with /boot/efi must be included in /dev/md1")
+#ensure(md1.filter_devices(secondary_disk), "Disk with /boot/efi2 must be included in /dev/md1")
 
 # TODO: implement the disk-picking algorithm
 enclosure_disk = query_multipath_devices()[0]
 info(f"Target enclosure disk: {enclosure_disk}")
 
-primary_disk_partitions = sorted(query_partitions(primary_disk), key=lambda k: k["size"])
-enclosure_partitions = sorted(query_partitions(enclosure_disk), key=lambda k: k["size"])
+partitions = resolve_partitions(primary_disk, enclosure_disk)
+print(partitions)
 
-# TODO: match partitions: firstly those without raid flag, then with raid falg
+move_boot_efi(primary_disk + partitions["efi"][0], enclosure_disk + partitions["efi"][1])
+move_raid_partition(md0, primary_disk + partitions["boot"][0], enclosure_disk + partitions["boot"][1])
+move_raid_partition(md1, primary_disk + partitions["lvm"][0], enclosure_disk + partitions["lvm"][1])
+
+
