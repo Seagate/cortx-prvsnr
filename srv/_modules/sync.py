09f492936e3df089f86db8eeeb4204d6b3def3e8
sradhanand.pati <sradhanand.pati@seagate.com>
2020-08-14 01:04:35 -0600

    Incorporating review comments
    Signed-off-by: sradhanand.pati <sradhanand.pati@seagate.com>

--- srv/_modules/sync.py
+++ srv/_modules/sync.py
@@ -1,7 +1,9 @@
 import os
 import yaml
+import logging
 import subprocess
-from cortx import _read_yaml
+
+log = logging.getLogger(__name__)
 
 def sync_files(component = "provisioner", replace_node = False):
     yaml_file = '/opt/seagate/cortx/{0}/conf/setup.yaml'.format(component)
@@ -12,26 +14,27 @@ def sync_files(component = "provisioner", replace_node = False):
     node = __grains__["id"]
     if not replace_node:
         replace_node = os.getenv("NODE_REPLACED", False)
-        node_list = __pillar__["cluster"]["node_list"]
-        node_list.remove(node)
-        node = node_list[0]
+
     if not replace_node:
         if node == "srvnode-1": node = "srvnode-2"
         else: node = "srvnode-1"
-        
-   
-    yaml_dict = _read_yaml(yaml_file)
-    if yaml_dict[component]["backup"] and yaml_dict[component]["backup"]["files"]:
+    else:
+        node_list = __pillar__["cluster"]["node_list"]
+        node_list.remove(node)
+        node = node_list[0]
+
+    with open(yaml_file, 'r') as fd:
+        yaml_dict = yaml.safe_load(fd)
+
+    if "backup" in yaml_dict[component] and "files" in yaml_dict[component]["backup"]:
         cmd = "rsync -zavhe ssh"
         for file in yaml_dict[component]["backup"]["files"]:
-            subprocess.run(
-                cmd.append(" {0}:{1} {1}".format(node,file)),
-                shell = True,
-                stdout = subprocess.PIPE
+            log.info(
+                subprocess.run(
+                    ["{0} {1}:{2} {2}".format(cmd,node,file)],
+                    shell = True,
+                    stdout = subprocess.PIPE
+                )
             )
     return True
-      
-      
-
-
 
