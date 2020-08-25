5531834274c271660a7b56215ca8640e76670293
Pritam Bhavsar <pritam.bhavsar@seagate.com>
2020-08-25 11:56:29 +0530

    Update cortx-prereqs.sh

--- cli/src/cortx-prereqs.sh
+++ cli/src/cortx-prereqs.sh
@@ -323,7 +323,12 @@ yum clean all >> ${LOG_FILE}
 echo "Done." 2>&1 | tee -a ${LOG_FILE} && sleep 1
 
 # Install lspci command
-yum install pciutils -y
+rpm -qa | grep -q pciutils && {
+    echo "INFO: pciutils package is already installed." 2>&1 | tee -a ${LOG_FILE}
+} || {
+    echo "INFO: Installing pciutils package" 2>&1 | tee -a ${LOG_FILE}
+    yum install -y pciutils 2>&1 | tee -a ${LOG_FILE}
+}
 if ( lspci -d"15b3:*"|grep Mellanox ) ; then 
     rpm -qa | grep -q mlnx-ofed-all && rpm -qa | grep -q mlnx-fw-updater && {
         echo "INFO: Mellanox Drivers are already installed." 2>&1 | tee -a ${LOG_FILE}
