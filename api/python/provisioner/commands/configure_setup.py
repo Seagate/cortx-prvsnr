dbb0e08fa51da1bd057a2182682d98f5a9a7981c
mazinamdar <mazhar.inamdar@seagate.com>
2020-09-12 02:37:44 -0600

    handle single values as array
    Signed-off-by: mazinamdar <mazhar.inamdar@seagate.com>

--- api/python/provisioner/commands/configure_setup.py
+++ api/python/provisioner/commands/configure_setup.py
@@ -214,6 +214,10 @@ class ConfigureSetup(CommandParserFillerMixin):
                 value = [f'\"{x.strip()}\"' for x in input[key].split(",")]
                 value = ','.join(value)
                 input[key] = f'[{value}]'
+            elif 'mgmt_nw.iface' in key:
+                # special case single value as array
+                # Need to fix this array having single value
+                input[key] = f'[\"{input[key]}\"]'
             else:
                 if input[key]:
                     if input[key] == 'None':
