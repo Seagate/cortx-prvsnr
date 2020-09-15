069852ebf47633c2929a1134017dc5e27ddaf627
Dmitry Didenko <dimitry.didenko@seagate.com>
2020-09-16 00:48:53 +0300

    EOS-12985: make '-'-symbol escaping permanently
    Signed-off-by: Dmitry Didenko <dimitry.didenko@seagate.com>

--- api/python/provisioner/api.py
+++ api/python/provisioner/api.py
@@ -294,9 +294,8 @@ def cmd_run(cmd_name: str, cmd_args: str = "", cmd_stdin: str = "",
     #      provisioner.cmd_run cortxcli --cmd-args -h
     #  But is should be
     #      provisioner.cmd_run cortxcli --cmd-args='-h'
-    if nowait:
-        if '-' in cmd_args:
-            cmd_args = f"'{cmd_args}'"
+    if '-' in cmd_args:
+        cmd_args = f"'{cmd_args}'"
     return _api_call('cmd_run', cmd_name, cmd_args=cmd_args,
                      cmd_stdin=cmd_stdin, targets=targets, nowait=nowait,
                      dry_run=dry_run)
