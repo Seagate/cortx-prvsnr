4900b17226e4eb6683d5b7928475487a42e0633c
Andrey Kononykhin <andrey.kononykhin@seagate.com>
2020-09-25 01:42:37 +0300

    Fix single node setup using setup_singlenode API (#502) (#516)
    * fix for singlenode setup
    
    Signed-off-by: mazinamdar <mazhar.inamdar@seagate.com>
    
    * RBOD and ther type does not require storage enclosure
    
    Signed-off-by: mazinamdar <mazhar.inamdar@seagate.com>
    
    Co-authored-by: mazinamdar <mazhar.inamdar@seagate.com>

--- api/python/provisioner/commands/configure_setup.py
+++ api/python/provisioner/commands/configure_setup.py
@@ -137,7 +137,8 @@ class StorageEnclosureParamsValidation:
 
     def __attrs_post_init__(self):
         params = attr.asdict(self)
-        if params['type'] == 'JBOD':
+        types = ['JBOD', 'virtual', 'RBOD', 'other']
+        if params['type'] in types:
             return
         missing_params = []
         for param, value in params.items():
