4cbc2c478af6ab116a717acb978a80020ecfe6c3
Andrey Kononykhin <andrey.kononykhin@seagate.com>
2020-11-11 12:48:50 +0300

    fixes merge error
    Signed-off-by: Andrey Kononykhin <andrey.kononykhin@seagate.com>

--- api/python/provisioner/commands/__init__.py
+++ api/python/provisioner/commands/__init__.py
@@ -199,13 +199,6 @@ class RunArgsConfigureCortx:
             }
         }, default=False
     )
-    local: bool = attr.ib(
-        metadata={
-            inputs.METADATA_ARGPARSER: {
-                'help': "use local salt client (salt-call --local)"
-            }
-        }, default=False
-    )
 
 
 @attr.s(auto_attribs=True)
