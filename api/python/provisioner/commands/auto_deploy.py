7a660c70db69cd9ad44c395d6fdebdf4479d80dd
Divya Vijayakumar <divya.vijaykumar@seagate.com>
2020-11-29 12:20:27 +0530

    EOS-14136: DecisionMaker class for factory deployment (#764)
    * EOS-14136: DecisionMaker class for factory deployment
    
    Signed-off-by: Divya Vijayakumar <divya.vijaykumar@seagate.com>
    
    * added replace nodes checks
    
    Signed-off-by: Divya Vijayakumar <divya.vijaykumar@seagate.com>
    
    * rebased and added codacy fixes
    
    Signed-off-by: Divya Vijayakumar <divya.vijaykumar@seagate.com>
    
    * indent docstring
    
    Signed-off-by: Divya Vijayakumar <divya.vijaykumar@seagate.com>
    
    * more docstring rules
    
    Signed-off-by: Divya Vijayakumar <divya.vijaykumar@seagate.com>

--- api/python/provisioner/commands/auto_deploy.py
+++ api/python/provisioner/commands/auto_deploy.py
@@ -18,10 +18,12 @@
 from typing import Type
 import logging
 
-from .. import (
-    config,
-    inputs
+from ..config import (
+    PRVSNR_PILLAR_CONFIG_INI,
+    GroupChecks
 )
+from .. import inputs
+
 from ..vendor import attr
 
 from . import (
@@ -31,6 +33,11 @@ from .setup_provisioner import (
     RunArgsSetupProvisionerGeneric,
     SetupProvisioner,
     SetupCmdBase
+)
+from .check import (
+    Check,
+    PreChecksDecisionMaker,
+    PostChecksDecisionMaker
 )
 from . import deploy_dual
 
@@ -44,6 +51,23 @@ class AutoDeploy(SetupCmdBase, CommandParserFillerMixin):
         RunArgsSetupProvisionerGeneric,
         deploy_dual.run_args_type
     ]
+
+
+    @staticmethod
+    def deployment_validations(deploy_check):
+        checker = Check()
+
+        try:
+            check_res = checker.run(deploy_check)
+        except Exception as exc:
+            raise ValueError("Error During Deployment "
+                            f"{deploy_check} Validations: {str(exc)}")
+        else:
+            if "pre" in deploy_check:
+                PreChecksDecisionMaker().make_decision(check_result=check_res)
+
+            else:
+                PostChecksDecisionMaker().make_decision(check_result=check_res)
 
     def run(self, nodes, **kwargs):
         setup_provisioner_args = {
@@ -66,7 +90,7 @@ class AutoDeploy(SetupCmdBase, CommandParserFillerMixin):
             setup_ctx.ssh_client.cmd_run(
                 (
                     'provisioner configure_setup '
-                    f'{config.PRVSNR_PILLAR_CONFIG_INI} '
+                    f'{PRVSNR_PILLAR_CONFIG_INI} '
                     f'{len(nodes)}'
                 ), targets=setup_ctx.run_args.primary.minion_id
             )
@@ -77,9 +101,15 @@ class AutoDeploy(SetupCmdBase, CommandParserFillerMixin):
                 ), targets=setup_ctx.run_args.primary.minion_id
             )
 
+        logger.info("Deployment Pre-Flight Validations")
+        self.deployment_validations(GroupChecks.DEPLOY_PRE_CHECKS.value)
+
         logger.info("Deploy")
         deploy_dual.DeployDual(setup_ctx=setup_ctx).run(
             **deploy_args
         )
+
+        logger.info("Post-Deployment Validations")
+        self.deployment_validations(GroupChecks.DEPLOY_POST_CHECKS.value)
 
         logger.info("Done")
