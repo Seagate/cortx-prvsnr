babaa14e3c16d201fe742cc6c6219a3d48cf2df3
Anjali Somwanshi <anjali.somwanshi@seagate.com>
2021-02-15 12:23:20 +0530

    EOS-17565: Update version and license for python36-cortx-prvsnr package
    Signed-off-by: Anjali Somwanshi <anjali.somwanshi@seagate.com>

--- api/python/provisioner/__metadata__.py
+++ api/python/provisioner/__metadata__.py
@@ -16,7 +16,7 @@
 #
 
 __title__ = 'cortx-prvsnr'
-__version__ = '0.42.0'
+__version__ = '2.42.0'
 __author__ = "Seagate"
 __author_email__ = 'support@seagate.com'  # TODO
 __maintainer__ = 'Seagate'
@@ -25,7 +25,7 @@ __url__ = 'https://github.com/Seagate/cortx-prvsnr'
 __description__ = 'Provisioner API for CORTX components'
 __long_description__ = __description__
 __download_url__ = f"{__url__}"
-__license__ = "GNU Affero General Public License"  # TODO
+__license__ = "Seagate"  # TODO Seagate
 
 __all__ = [
     '__title__',
