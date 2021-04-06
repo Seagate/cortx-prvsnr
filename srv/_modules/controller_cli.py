0cc8ba4559f9f8130478e9f4b87ab8fc1f24bba5
Pritam Bhavsar <pritam.bhavsar@seagate.com>
2021-04-06 11:57:33 +0530

    EOS-18903: add missing file - srv/_modules/controller_cli.py
    Signed-off-by: Pritam Bhavsar <pritam.bhavsar@seagate.com>

---
+++ srv/_modules/controller_cli.py
@@ -0,0 +1,90 @@
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+
+# How to test:
+# $ salt-call saltutil.clear_cache
+# $ salt-call saltutil.sync_modules
+# $ salt-call controller_cli.fetch_enclosure_serial
+# On successfull run, the fetched enclosure id/serial is kept at:
+# /etc/enclosure_id file. 
+
+# Standard packages
+import logging
+import os
+import subprocess
+import sys
+
+#from shutil import copyfile
+
+# Update PYTHONPATH to include Provisioner API installed at:
+# /usr/local/lib/python3.6/site-packages/cortx-prvsnr-*
+sys.path.append(os.path.join('usr','local','lib', 'python3.6', 'site-packages'))
+
+# Cortx packages
+import provisioner
+
+from pathlib import Path
+logger = logging.getLogger(__name__)
+
+def fetch_enclosure_serial():
+
+    current_node = __grains__['id']
+
+    current_enclosure = "enclosure-" + ((current_node).split('-'))[1]
+    ctrl_cli_utility = __pillar__['provisioner']['storage']['controller']['cli_utility_path']
+
+    host = __pillar__['storage'][current_enclosure ]['controller']['primary']['ip']
+    user = __pillar__['storage'][current_enclosure]['controller']['user']
+    secret = __pillar__['storage'][current_enclosure]['controller']['secret']
+    logs = "/var/log/seagate/provisioner/controller-cli.log"
+    _opt = "--show-license"
+
+    logger.info("[ INFO ] Running controller-cli utility to get enclosure serial...")
+    _cmd = f"sh {ctrl_cli_utility} host -h {host} -u {user} -p '{secret}' {_opt} | grep -A2 Serial | tail -1 > /etc/enclosure_id"
+    subprocess.Popen([_cmd],shell=True,stdout=subprocess.PIPE).stdout.read().decode("utf-8").splitlines()
+
+    _enc_id_file = Path('/etc/enclosure_id')
+    if not _enc_id_file.exists():
+        msg = f"ERROR: Could not generate the enclosure id from controller cli utility, please check the {str(logs)} for more details"
+        # raise Exception(msg)
+        logger.error(msg)
+        return False
+    else:
+        # Check if file /etc/enclosure_id has correct content:
+        # 1. has only one line and
+        # 2. has only one word - enclosure serial.
+        with open(_enc_id_file) as _fp:
+            _line_cnt = 0
+            _words = 0
+            for _line in _fp:
+                logger.info(f"content of line {_line_cnt}: {_line}")
+                _words = _line.strip()
+                _line_cnt += 1
+
+        _n_words = 1
+        for i in _words:
+            if i == " ":
+               _n_words += 1
+
+        if ((_line_cnt > 1) or (_n_words > 1)):
+            msg = "ERROR: The contents of /etc/enclosure_id looks incorrect, failing"
+            logger.error(msg)
+            return False
+
+    logger.info("Enclosure id generated successfully and is kept at /etc/enclosure_id")
+
+    return True
