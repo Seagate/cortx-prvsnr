3323938e96159c168857ef482155d700c0baf32e
Andrey Kononykhin <andrey.kononykhin@seagate.com>
2021-04-07 12:14:37 +0300

    removes explicit dependency link for api rpm
    Signed-off-by: Andrey Kononykhin <andrey.kononykhin@seagate.com>

--- devops/rpms/api/build_python_api.sh
+++ devops/rpms/api/build_python_api.sh
@@ -183,7 +183,9 @@ if [[ "$in_docker" == true ]]; then
     output_dir="/tmp/out"
 fi
 
+    # FIXME make deps on salt and packages explicit
     # --depends "salt >= 3002" \
+    # --depends "salt >= 20.4" \
 $fpm_tool --input-type "python" \
     --output-type "$output_type" \
     --architecture "amd64" \
@@ -193,6 +195,7 @@ $fpm_tool --input-type "python" \
     --python-package-name-prefix "python36" \
     --python-bin "python3" \
     --python-disable-dependency salt \
+    --python-disable-dependency packaging \
     --no-python-downcase-dependencies \
     --exclude "*.pyc" \
     --exclude "*.pyo" \
