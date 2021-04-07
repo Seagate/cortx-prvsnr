2103f2a9cbfcaf05c4128aba9449b035f5dbf480
Andrey Kononykhin <andrey.kononykhin@seagate.com>
2021-04-07 20:25:58 +0300

    improves test debug routine
    Signed-off-by: Andrey Kononykhin <andrey.kononykhin@seagate.com>

--- test/build_helpers/build_env/test_build_env.py
+++ test/build_helpers/build_env/test_build_env.py
@@ -24,17 +24,6 @@ from copy import deepcopy
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.debug
-def test_fail(test_level, test_topic):
-    pytest.fail()
-
-@pytest.mark.debug
-def test_pass(test_level, test_topic):
-    pass
-
-
-
-
 @pytest.fixture
 def env_provider():
     return 'docker'
