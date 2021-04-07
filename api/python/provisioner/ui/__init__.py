46dfe52168a55a2f92405d729fa9ba66fd818090
mazinamdar <mazhar.inamdar@seagate.com>
2021-04-07 06:44:26 -0600

    updated cortx-prvsnr pythonpackage
    Signed-off-by: mazinamdar <mazhar.inamdar@seagate.com>

--- api/python/provisioner/ui/__init__.py
+++ api/python/provisioner/ui/__init__.py
@@ -1,6 +1,4 @@
-#
 # Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
-#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
