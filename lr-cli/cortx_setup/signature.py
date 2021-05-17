7bef291136947f53077acb95315246c9cd7284f7
Divya Vijayakumar <divya.vijaykumar@seagate.com>
2021-05-18 01:45:29 +0530

    set node Signature directlt to ConfStore
    Signed-off-by: Divya Vijayakumar <divya.vijaykumar@seagate.com>

--- lr-cli/cortx_setup/signature.py
+++ lr-cli/cortx_setup/signature.py
@@ -18,20 +18,22 @@
 # Cortx Setup API for Node Stamping Signature operations
 
 
+import argparse
 import logging
+import subprocess
+import yaml
 
-from provisioner.commands import (
-     PillarGet,
-     PillarSet
-)
-from provisioner.salt import local_minion_id
+from pathlib import Path
+
+# CORTX package
+from cortx.utils.conf_store import Conf
 
 logger = logging.getLogger(__name__)
 
 
 class Signature:
 
-    def get(key=None):
+    def get(**kwargs):
         """signature get command execution method.
 
         (Specific only to current(given) node.)
@@ -42,33 +44,35 @@ class Signature:
 
         """
         try:
-            targets = local_minion_id()
-            node_data = PillarGet().run(
-                          "node", targets=targets
-                          )[targets]
+            index = 'signature'
+            lr_sign_file = Path("/etc/node-signature.yaml")
 
-            # Reference: cortx_setup/sample.json
+            if not (lr_sign_file.exists() and
+                    lr_sign_file.stat().st_size != 0):
 
-            if 'signature' not in node_data['node'][targets]:
-                user_msg = ("Node Signature was never set in database. "
-                            "Execute `signature set` command to set it.")
-                logger.error(user_msg)
-                raise Exception(user_msg)
+                logger.warning("Node Signature was never set in database. "
+                               "Set it first with `signature set` command.")
+                lr_sign = "No data found. Execute `signature set` command."
 
-            if key:
-                lr_sign = node_data['node'][targets]['signature'][key]
             else:
-                lr_sign = node_data['node'][targets]['signature']
+                Conf.load(index, f'yaml://{lr_sign_file}')
+                lr_sign = Conf.get(index, f'signature>{kwargs["key"]}')
+
+                if lr_sign is None:
+                    logger.warning(
+                       f"Given key '{kwargs['key']}' is possibly "
+                       "not found in Signature data."
+                    )
+                    lr_sign = Conf.get(index, 'signature')
 
             return f"LR Signature: {lr_sign}"
 
         except ValueError as exc:
             raise ValueError(
-                "Failed: Encountered error while retrieving "
-                f"LR signature from database: {str(exc)}"
+              f"Failed: GET LR signature from ConfStore: {str(exc)}"
             )
 
-    def set(key=None, value=None, signature=None):
+    def set(**kwargs):
         """signature set command execution method.
 
         (Specific only to current(given) node.)
@@ -76,50 +80,90 @@ class Signature:
 
         Execution:
         `cortx_setup signature set --key 'key' --value 'value'`
-        OR
-        `cortx_setup signature set --signature {'key': 'value'}`
 
         """
         try:
-            targets = local_minion_id()
-            key_path = f"node/{targets}/signature"
-            lr_sign = {}
-
-            node_data = PillarGet().run("node", targets=targets)[targets]
-
-            if 'signature' in node_data['node'][targets]:
-                lr_sign = node_data['node'][targets]['signature']
+            index = 'signature'
+            lr_sign_file = Path("/etc/node-signature.yaml")
 
             if key and value:
-                lr_sign.update({key: value})
+                if not (lr_sign_file.exists() and
+                        lr_sign_file.stat().st_size != 0):
 
-            elif signature:
-                if not isinstance(signature, dict):
-                    user_msg = ("Expected dictionary format for "
-                                "node signature: {'key': 'value'}")
-                    logger.error(user_msg)
-                    raise Exception(user_msg)
+                    logger.warning("Node Signature was never set "
+                                   "in ConfStore. Setting now.")
+                    data = {'signature': {}}
 
-                lr_sign.update(signature)
+                else:
+                    # Immutable file. Make it editable first.
+                    _cmd = f"chattr -i {lr_sign_file}"
+                    subprocess.Popen(_cmd, shell=True)     # nosec
+
+                    with open(lr_sign_file, 'r') as fo:
+                        data = yaml.load(fo, yaml.SafeLoader)
+
+                Conf.load(index, f'yaml://{lr_sign_file}')
+                data['signature'].update({key: value})
+
+                with open(lr_sign_file, 'w') as fi:
+                    yaml.dump(data, fi, default_flow_style=False)
+
+                Conf.set(index, f'signature>{kwargs["key"]}', f'{kwargs["value"]}')
+                Conf.save(index)
+
+                # TODO: With refined approach, check if this signature data
+                # should be written to grains. If yes, what format?
+
+                # Make the file immutable for further editing
+                _cmd = f"chattr +i {lr_sign_file} && lsattr {lr_sign_file}"
+                subprocess.Popen(_cmd, shell=True)     # nosec
 
             else:
-                user_msg = ("A valid signature format is mandatory to set for stamping. "
-                            "Expected Signature formats: \"{'key': 'value'} \" "
-                            "or \"--key 'key' --value 'value' \"."
+                user_msg = ("A valid signature is mandatory to set for stamping. "
+                            "Expected Signature format: \"--key 'key' --value 'value' \"."
                             )
                 logger.error(user_msg)
                 raise Exception(user_msg)
 
-            PillarSet().run(
-                   key_path,
-                   lr_sign,
-                   targets=targets
-                )
-
-            return "Success: Stamping done for current node and database updated."
+            return "Success: Stamping done for current node and ConfStore updated."
 
         except Exception as exc:
             raise ValueError(
-                "Failed: Encountered error while setting "
-                f"LR signature to database: {str(exc)}"
+              f"Failed: SET LR signature to ConfStore: {str(exc)}"
             )
+
+
+if __name__ == "__main__":
+
+    # This entire section will be converted (with better design)
+    # to a common utility to suit all Factory functionalities:
+    # signature, network, storage, node_init, etc.
+
+    parser = argparse.ArgumentParser(
+                   description='For Node Signature')
+    parser.add_argument('--get',
+                        nargs='*',
+                        type=str,
+                        help='Gets the given signature value from database')
+    parser.add_argument('--set',
+                        nargs='*',
+                        type=str,
+                        help='Sets the given signature key:value to database')
+
+    args = vars(parser.parse_args())
+
+    parsed_conf = {}
+    for pair in (args['set'] or args['get']):
+        key, value = pair.split('=')
+        parsed_conf[key] = value
+
+    if args['get']:
+        resp = Signature.get(
+           key=parsed_conf['key']
+        )
+    elif args['set']:
+        resp = Signature.set(
+            key=parsed_conf['key'],
+            value=parsed_conf['value']
+        )
+    print(resp)
