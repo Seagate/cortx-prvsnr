9b5048e5f125f832310ad793d4b4cf8bc885988d
Yashodhan Pise <yashodhan.pise@seagate.com>
2021-05-19 00:13:36 +0530

    Refatoring to lower complexity.
    Signed-off-by: Yashodhan Pise <yashodhan.pise@seagate.com>

--- lr-cli/cortx_setup/signature.py
+++ lr-cli/cortx_setup/signature.py
@@ -32,9 +32,11 @@ logger = logging.getLogger(__name__)
 
 
 class Signature:
+    
+    def __init__(self):
+        lr_signature_file_path = Path("/etc/node-signature.yaml")
 
-    @staticmethod
-    def get(**kwargs):
+    def get(self, **kwargs):
         """signature get command execution method.
 
         (Specific only to current(given) node.)
@@ -46,7 +48,7 @@ class Signature:
         """
         try:
             index = 'signature'
-            lr_sign_file = Path("/etc/node-signature.yaml")
+            lr_sign_file = self.lr_signature_file_path
 
             if not (lr_sign_file.exists() and
                     lr_sign_file.stat().st_size != 0):
@@ -59,7 +61,7 @@ class Signature:
                 Conf.load(index, f'yaml://{lr_sign_file}')
                 lr_sign = Conf.get(index, f'signature>{kwargs["key"]}')
 
-                if lr_sign is None:
+                if not lr_sign:
                     logger.warning(
                        f"Given key '{kwargs['key']}' is possibly "
                        "not found in Signature data."
@@ -73,8 +75,7 @@ class Signature:
               f"Failed: GET LR signature from ConfStore: {str(exc)}"
             )
 
-    @staticmethod
-    def set(**kwargs):
+    def set(self, **kwargs):
         """signature set command execution method.
 
         (Specific only to current(given) node.)
@@ -86,30 +87,25 @@ class Signature:
         """
         try:
             index = 'signature'
-            lr_sign_file = Path("/etc/node-signature.yaml")
+            lr_sign_file = self.lr_signature_file_path
 
-            if key and value:
-                if not (lr_sign_file.exists() and
-                        lr_sign_file.stat().st_size != 0):
+            if not (key and value):
+                user_msg = ("A valid signature is mandatory to set for stamping. "
+                    "Expected Signature format: \"--key 'key' --value 'value' \"."
+                )
+                logger.error(user_msg)
+                raise Exception(user_msg)
 
-                    logger.warning("Node Signature was never set "
-                                   "in ConfStore. Setting now.")
-                    data = {'signature': {}}
-
-                else:
-                    # Immutable file. Make it editable first.
-                    _cmd = f"chattr -i {lr_sign_file}"
-                    subprocess.Popen(_cmd, shell=True)     # nosec
-
-                    with open(lr_sign_file, 'r') as fo:
-                        data = yaml.safe_load(fo)
+            if (lr_sign_file.exists() and
+                lr_sign_file.stat().st_size != 0
+            ):
+                # Immutable file. Make it editable first.
+                _cmd = f"chattr -i {lr_sign_file}"
+                subprocess.Popen(_cmd, shell=True)     # nosec
 
                 Conf.load(index, f'yaml://{lr_sign_file}')
-                data['signature'].update({key: value})
-
-                with open(lr_sign_file, 'w') as fi:
-                    yaml.dump(data, fi, default_flow_style=False)
-
+                Conf.set('signature', key, value)
+            
                 Conf.set(index, f'signature>{kwargs["key"]}', f'{kwargs["value"]}')
                 Conf.save(index)
 
@@ -119,13 +115,6 @@ class Signature:
                 # Make the file immutable for further editing
                 _cmd = f"chattr +i {lr_sign_file} && lsattr {lr_sign_file}"
                 subprocess.Popen(_cmd, shell=True)     # nosec
-
-            else:
-                user_msg = ("A valid signature is mandatory to set for stamping. "
-                            "Expected Signature format: \"--key 'key' --value 'value' \"."
-                            )
-                logger.error(user_msg)
-                raise Exception(user_msg)
 
             return "Success: Stamping done for current node and ConfStore updated."
 
@@ -160,11 +149,11 @@ if __name__ == "__main__":
         parsed_conf[key] = value
 
     if args['get']:
-        resp = Signature.get(
+        resp = Signature().get(
            key=parsed_conf['key']
         )
     elif args['set']:
-        resp = Signature.set(
+        resp = Signature().set(
             key=parsed_conf['key'],
             value=parsed_conf['value']
         )
