f528c3e383d0c334ebf588bbfb9fe14caf1148cf
Yashodhan Pise <yashodhan.pise@seagate.com>
2021-05-26 14:01:53 +0530

    EOS-20522: Bootstrap script require firewall fixes for OVA 1.0.4
    Signed-off-by: Yashodhan Pise <yashodhan.pise@seagate.com>

--- cli/src/virtual_appliance/bootstrap.sh
+++ cli/src/virtual_appliance/bootstrap.sh
@@ -46,6 +46,11 @@ salt-call state.apply components.system.config.hosts
 #reconfigure firewall
 salt-call state.apply components.system.firewall.teardown
 salt-call state.apply components.system.firewall
+# Firewall Salt states correct all deviations on the system
+# OVA environment needs corrections to firewall rules
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --zone=public --add-port=443/tcp --permanent
+firewall-cmd --reload
 
 #reconfigure lustre
 salt-call state.apply components.misc_pkgs.lustre.stop
