fef0ed615921cd1bdafddb19bb69d22a6a94ab25
Pritam Bhavsar <pritam.bhavsar@seagate.com>
2021-05-27 11:14:30 +0530

    Add import for storage config and commons
    Signed-off-by: Pritam Bhavsar <pritam.bhavsar@seagate.com>

--- lr-cli/cortx_setup/commands/__init__.py
+++ lr-cli/cortx_setup/commands/__init__.py
@@ -26,6 +26,8 @@ from .security.config import SecurityConfig
 from .resource.show import ResourceShow
 from .resource.discover import ResourceDiscover
 from .node.prepare.firewall import NodePrepareFirewall
+from .storage.config import StorageEnclosureConfig
+from .storage.commons import Commons
 
 
 __all__ = [
