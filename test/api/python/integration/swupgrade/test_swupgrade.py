d044a8648e55ea43e93f9ac6beb87bf5800c298e
Andrey Kononykhin <andrey.kononykhin@seagate.com>
2021-06-01 16:10:22 +0300

    configures sw upgrade test to run 3 nodes
    Signed-off-by: Andrey Kononykhin <andrey.kononykhin@seagate.com>

--- test/api/python/integration/swupgrade/test_swupgrade.py
+++ test/api/python/integration/swupgrade/test_swupgrade.py
@@ -83,7 +83,7 @@ def mock_system_cmds(hosts, request):
 # main goals:
 #   - verify delegation of orchestration to a new version
 #   - verify salt based call work fine (mini API)
-@pytest.mark.hosts_num(1)
+@pytest.mark.hosts_num(3)
 @pytest.mark.verified
 @pytest.mark.isolated
 def test_swupgrade_r2_offline(
