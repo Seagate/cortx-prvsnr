7ef35518ba0477536a54a4fdbbb252ac3033b590
Tanuja Shinde <tanuja.shinde@seagate.com>
2021-06-08 21:28:48 +0530

    updated cluster-id creation logic
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- api/python/provisioner/commands/setup_provisioner.py
+++ api/python/provisioner/commands/setup_provisioner.py
@@ -791,19 +791,6 @@ class SetupProvisioner(SetupCmdBase, CommandParserFillerMixin):
         else:
             masters = load_yaml(masters_pillar_path)
 
-        # TODO IMPROVE many hard coded values
-
-        cluster_id_path = all_minions_dir / 'cluster_id'
-
-        # TODO: This new cluster_id generation step must be removed
-        # as it is handled in last step of bootstrap (cluster_id API)
-
-        if not cluster_id_path.exists():
-            cluster_uuid = str(uuid.uuid4())
-            dump_yaml(cluster_id_path, dict(cluster_id=cluster_uuid))
-        else:
-            cluster_uuid = load_yaml(cluster_id_path)['cluster_id']
-
         #   TODO IMPROVE EOS-8473 use salt caller and file-managed instead
         #   (locally) prepare minion config
         #   FIXME not valid for non 'local' source
@@ -878,7 +865,6 @@ class SetupProvisioner(SetupCmdBase, CommandParserFillerMixin):
                                 'primary' if (node is run_args.primary)
                                 else 'secondary'
                             ]},
-                            {'cluster_id': cluster_uuid},
                             {'node_id': node_uuid},
                             {'hostname_status': hostnamectl_status},
                         ]
