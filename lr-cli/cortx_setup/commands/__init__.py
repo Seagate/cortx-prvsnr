557291f10d487a9c902b354eb3ac3d0957248fcc
Pritam Bhavsar <pritam.bhavsar@seagate.com>
2021-07-23 14:42:31 +0530

    EOS-21906: Support for Service Config Keys (S3 Server Instances) (#5985)
    * EOS-21906 Adding new api config
    
    Signed-off-by: zahid.shaikh <zahid.shaikh@seagate.com>
    
    * EOS-21906 Adding new api config
    
    Signed-off-by: zahid.shaikh <zahid.shaikh@seagate.com>
    
    * EOS-21906 uncommenting temp changes
    
    Signed-off-by: zahid.shaikh <zahid.shaikh@seagate.com>
    
    * EOS-21906 adding template file
    
    Signed-off-by: zahid.shaikh <zahid.shaikh@seagate.com>
    
    * EOS-21906 correction appended
    
    Signed-off-by: zahid.shaikh <zahid.shaikh@seagate.com>
    
    * Address review comment
    
    Signed-off-by: Pritam Bhavsar <pritam.bhavsar@seagate.com>
    
    * Address review comments
    
    Signed-off-by: Pritam Bhavsar <pritam.bhavsar@seagate.com>
    
    * Remove unwanted changes which got in while resolving conflicts
    
    Signed-off-by: Pritam Bhavsar <pritam.bhavsar@seagate.com>
    
    * Remove unwanted changes
    
    Signed-off-by: Pritam Bhavsar <pritam.bhavsar@seagate.com>
    
    Co-authored-by: zahid.shaikh <zahid.shaikh@seagate.com>

--- lr-cli/cortx_setup/commands/__init__.py
+++ lr-cli/cortx_setup/commands/__init__.py
@@ -47,6 +47,8 @@ from .storageset.add.node import AddServerNode
 from .storageset.add.enclosure import AddStorageEnclosure
 from .storageset.config.durability import DurabilityConfig
 from .enclosure.refresh import RefreshEnclosureId
+from .config.set import SetConfiguration
+from .config.get import GetConfiguration
 
 
 __all__ = [
@@ -81,5 +83,7 @@ __all__ = [
     'StorageEnclosureConfig',
     'SaltCleanup',
     'PrepareConfstore',
-    'PillarSync'
+    'PillarSync',
+    'SetConfiguration',
+    'GetConfiguration'
  ]
---
+++ lr-cli/cortx_setup/commands/config/get.py
@@ -0,0 +1,57 @@
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com."
+#
+
+from ..command import Command
+
+# CORTX package
+from cortx.utils.conf_store import Conf
+from cortx_setup.config import CONFSTORE_CLUSTER_FILE
+
+
+class GetConfiguration(Command):
+    _args = {
+        'key': {
+            'type': str,
+            'default': None,
+            'optional': True,
+            'help': 'Configuration details to retrieve for this key'
+        }
+    }
+
+    def run(self, key=None):
+        """
+        To get the value of specific key
+        :param key: key
+        :return: Value read from confstore
+        :Execution:
+            'cortx_setup config get --key <key>'
+        """
+        if not key:
+            raise ValueError(
+               "Invalid input. Expected Config key to get the value in format: --key 'key'."
+            )
+
+        index = 'node_info_index'
+        self.load_conf_store(index, f'json://{CONFSTORE_CLUSTER_FILE}')
+        try:
+            val_fr_conf = Conf.get(index, key)
+            self.logger.debug(f"Config value for '{key}' : {val_fr_conf}")
+            return val_fr_conf
+        except ValueError as exc:
+            raise ValueError(
+              "Failed to get config value from ConfStore: %s" % str(exc)
+            )
