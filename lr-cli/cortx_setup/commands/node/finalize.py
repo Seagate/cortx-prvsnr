985189adfebb7bf3d7e8fee0e352326c1442a10e
Divya Vijayakumar <divya.vijaykumar@seagate.com>
2021-07-26 12:02:17 +0530

    EOS-22267: support user to login directly inside nodecli
    Signed-off-by: Divya Vijayakumar <divya.vijaykumar@seagate.com>

--- lr-cli/cortx_setup/commands/node/finalize.py
+++ lr-cli/cortx_setup/commands/node/finalize.py
@@ -203,7 +203,7 @@ class NodeFinalize(Command):
                     targets=node_id
                 )
 
-                self.logger.debug("Ensuring user permissions on system")
+                self.logger.debug(f"Ensuring {user} user permissions on system")
 
                 StateFunExecuter.execute(
                     'file.append',
@@ -211,6 +211,18 @@ class NodeFinalize(Command):
                         name=f'{home_dir}/.profile',
                         text=('sudo /usr/bin/nodecli \n'
                               'alias exit=exit;exit')
+                    ),
+                    targets=node_id
+                )
+
+            else:
+                self.logger.debug(f"Ensuring {user} user permissions on system")
+
+                StateFunExecuter.execute(
+                    'file.append',
+                    fun_kwargs=dict(
+                        name=f'{home_dir}/.profile',
+                        text='sudo /usr/bin/nodecli'
                     ),
                     targets=node_id
                 )
