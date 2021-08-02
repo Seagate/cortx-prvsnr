fc6925860b47e91ce8428347a3ef4fe14f8cc2de
sradhanandpati <sradhanand.pati@seagate.com>
2021-08-02 22:13:43 +0530

    EOS-22259: Populate DIX Pool layout parameters to confstore file (#6017)
    * EOS-22259: Populate DIX Pool layout parameters to confstore file
    
    Signed-off-by: sradhanandpati <sradhanand.pati@seagate.com>
    
    * EOS-22259: Update spare default value for VM and HW
    
    Signed-off-by: sradhanandpati <sradhanand.pati@seagate.com>
    
    * EoS-22259: Reverting changes for generic storage set as it required changes to config.ini
    
    Signed-off-by: sradhanandpati <sradhanand.pati@seagate.com>

--- lr-cli/cortx_setup/commands/storageset/config/durability.py
+++ lr-cli/cortx_setup/commands/storageset/config/durability.py
@@ -25,6 +25,14 @@ from cortx_setup.commands.common_utils import (
 )
 
 from provisioner.commands import PillarSet
+from provisioner.config import (
+    CONFSTORE_ROOT_FILE,
+    ALL_MINIONS
+)
+from provisioner.salt import (
+    StateFunExecuter,
+    copy_to_file_roots
+)
 from cortx.utils.conf_store import Conf
 
 
@@ -37,26 +45,27 @@ class DurabilityConfig(Command):
         },
         'type': {
             'type': str,
-            'default': 'sns',
+            'default': None,
             'optional': True,
             'dest': 'durability_type',
+            'choices': ['sns', 'dix'],
             'help': 'Storageset durability type. e.g: {sns|dix}'
         },
         'data': {
             'type': int,
-            'default': 4,
+            'default': None,
             'optional': True,
             'help': 'Storageset durability data'
         },
         'parity': {
             'type': int,
-            'default': 2,
+            'default': None,
             'optional': True,
             'help': 'Storageset durability parity value'
         },
         'spare': {
             'type': int,
-            'default': 2,
+            'default': None,
             'optional': True,
             'help': 'Storageset durability spare'
         }
@@ -64,52 +73,75 @@ class DurabilityConfig(Command):
 
     def run(self, **kwargs):
         try:
-            index = 'storage_durability_index'
+            confstore_config = 'storage_durability_index'
             storage_set_name = kwargs['storage_set_name']
             durability_type = kwargs['durability_type']
+
+            if None in [durability_type, kwargs['data'], kwargs['parity'], kwargs['spare']]:
+                raise Exception(
+                    "ERROR: Insufficient command parameters type specified")
+
             cluster_id = get_cluster_id()
+            durability_dict = {
+                'data': str(kwargs['data']),
+                'parity': str(kwargs['parity']),
+                'spare': str(kwargs['spare'])
+            }
 
             self.load_conf_store(
-                index,
+                confstore_config,
                 f'json://{CONFSTORE_CLUSTER_FILE}'
             )
 
-            ss_name = Conf.get(index,
-                               f'cluster>{cluster_id}>storage_set[0]>name')
+            storage_set_info = Conf.get(
+                confstore_config, f'cluster>{cluster_id}>storage_set')
+            storage_set_index = -1
 
-            if ss_name != storage_set_name:
+            for index, _ in enumerate(storage_set_info):
+                if storage_set_info[index]['name'] == storage_set_name:
+                    storage_set_index = index
+                    break
+
+            if storage_set_index == -1:
                 raise ValueError(
-                   "Invalid Storageset name provided: "
-                   f"'{storage_set_name}' not found in ConfStore data. "
-                   "First, set with `cortx_setup storageset create` command."
+                    f"Error: Storage-set '{storage_set_name}' not found."
+                    "Action: Configure with 'cortx_setup storageset create' command"
                 )
-            durability_key = f'cluster>{cluster_id}>storage_set[0]>durability'
-            durability_info = Conf.get(index, durability_key)
-            if not durability_info.get(durability_type, False):
-                durability_info[durability_type] = {}
-            for key, value in kwargs.items():
-                if key not in ['storage_set_name', 'durability_type']:
-                    self.logger.debug(
-                        f"Updating {key} as {value} in ConfStore"
-                    )
-                    durability_info[durability_type].update({key: value})
+
+            confstore_durability_key = f'cluster>{cluster_id}>storage_set[{storage_set_index}]>durability'
+            pillar_durability_key = 'cluster/srvnode-1/storage/durability'
 
             PillarSet().run(
-                'cluster/storage_set/durability',
-                durability_info
+                f'{pillar_durability_key}/{durability_type}',
+                durability_dict
             )
             Conf.set(
-                index,
-                f'cluster>{cluster_id}>storage_set[0]>durability',
-                durability_info
+                confstore_config,
+                f'{confstore_durability_key}>{durability_type}',
+                durability_dict
             )
-            Conf.save(index)
+            Conf.save(confstore_config)
             self.logger.debug(
                 f"Durability configured for Storageset '{storage_set_name}'"
             )
 
+            # Copy the current confstore across all nodes
+            dest = copy_to_file_roots(
+                CONFSTORE_CLUSTER_FILE, CONFSTORE_ROOT_FILE)
+            self.logger.debug("Confstore file provisioner root directory")
+
+            StateFunExecuter.execute(
+                'file.managed',
+                fun_kwargs=dict(
+                    name=str(CONFSTORE_CLUSTER_FILE),
+                    source=str(dest),
+                    makedirs=True
+                ),
+                targets=ALL_MINIONS
+            )
+            self.logger.debug("Confstore copied across all nodes of cluster")
+
         except ValueError as exc:
             raise ValueError(
-              f"Failed to configure durability. Reason: {str(exc)}"
+                f"Failed to configure durability. Reason: {str(exc)}"
             )
-
