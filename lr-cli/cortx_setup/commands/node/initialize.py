1b17a03028fd306636f48141f0e183612b8161c1
zahid.shaikh@seagate.com <zahid.shaikh@seagate.com>
2021-08-18 14:37:45 +0000

    Revert "WIP EOS-23583 Make corxt_setup node initialize command work for only iopath states"
    This reverts commit c4d3762116c9657fc1b24736d38a87d082975aa1.

--- lr-cli/cortx_setup/commands/node/initialize.py
+++ lr-cli/cortx_setup/commands/node/initialize.py
@@ -27,60 +27,26 @@ from cortx_setup.commands.common_utils import (
 
 
 class NodeInitialize(Command):
-    _args = {
-        'components': {
-            'type': str,
-            'default': None,
-            'optional': True,
-            'help': 'Components to be initialize (comma separated)'
-        }
-    }
+    _args = {}
 
     """Initialize cortx components by calling post_install command"""
 
-    def run(self, components=None):
+    def run(self):
         node_id = local_minion_id()
-        cmd_run(f"salt {node_id} saltutil.sync_all")
         cortx_components = get_cortx_states()
-        defined_comp_list = []
-        list(map(defined_comp_list.extend, cortx_components.values()))
-
-        if components:
-            self.logger.debug(f"Executing Node initialize for given "
-                              f"components {components}")
-            components = [component for component in components.split(",")
-                          if component and len(component) > 1]
-            for state in components:
-                if state in defined_comp_list:
-                    try:
-                        self.logger.debug(
-                            f"Executing post_install command for {state} "
-                            f"component"
-                        )
-                        print(state)
-                        deploy.Deploy()._apply_state(
-                            f"components.{state}",
-                            targets=node_id,
-                            stages=['config.post_install']
-                        )
-                    except Exception as ex:
-                        raise ex
-                else:
-                    self.logger.warning(f"Invalid component : '{state}'")
-        else:
-            for component in cortx_components:
-                states = cortx_components[component]
-                for state in states:
-                    try:
-                        self.logger.debug(
-                            f"Executing post_install command for {state} "
-                            f"component"
-                        )
-                        deploy.Deploy()._apply_state(
-                            f"components.{state}",
-                            targets=node_id,
-                            stages=['config.post_install']
-                        )
-                    except Exception as ex:
-                        raise ex
+        cmd_run(f"salt {node_id} saltutil.sync_all")
+        for component in cortx_components:
+            states = cortx_components[component]
+            for state in states:
+                try:
+                    self.logger.debug(
+                        f"Executing post_install command for {state} component"
+                    )
+                    deploy.Deploy()._apply_state(
+                        f"components.{state}",
+                        targets=node_id,
+                        stages=['config.post_install']
+                    )
+                except Exception as ex:
+                    raise ex
         self.logger.debug("Done")
