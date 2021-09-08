4e601506d47383de160afeac5fceca25ec876677
Ujjwal Lanjewar <ujjwal.lanjewar@seagate.com>
2021-09-08 23:05:25 +0530

    CORTX Provisioner Framework Skeleton (Build Scripts))
    Signed-off-by: Ujjwal Lanjewar <ujjwal.lanjewar@seagate.com>
    Signed-off-by: Shriya Deshmukh <shriya.deshmukh@seagate.com>

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,11 +1,59 @@
-#!/bin/bash -x
+#!/bin/bash
+#
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
 
-SCRIPT_DIR="$(dirname $0)"
-#IMAGE=${1:-corxt-provisioner}
+set -e
+PROG=$(basename "$0")
+SCRIPT_DIR=$(realpath $(dirname "$0"))
+BASE_DIR=$SCRIPT_DIR/..
+BUILD_NUMBER=
+GIT_VER=
 
-BUILD_URL=http://cortx-storage.colo.seagate.com/releases/cortx/github/main/centos-7.9.2009/last_successful_prod
+usage() {
+    echo """usage: $PROG[-v version] [-g git_version] [-b build_number]""" 1>&2;
+    exit 1;
+}
 
-#docker build -t $IMAGE .
-rpm -qa | grep -q docker-compose || yum install docker-compose
-docker-compose -f $SCRIPT_DIR/docker-compose.yml build --force-rm \
-  --build-arg BUILD_URL=$BUILD_URL cortx-provisioner
+# Check for passed in arguments
+while getopts ":g:v:b:" o; do
+    case "${o}" in
+        v)
+            VER=${OPTARG}
+            ;;
+        g)
+            GIT_VER=${OPTARG}
+            ;;
+        b)
+            BUILD_NUMBER=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+[ -z $"$GIT_VER" ] && GIT_VER="$(git rev-parse --short HEAD)" \
+        || GIT_VER="${GIT_VER}_$(git rev-parse --short HEAD)"
+[ -z "$VER" ] && VER="2.0.0"
+[ -z "$BUILD_NUMBER" ] && BUILD_NUMBER=1
+REL="${BUILD_NUMBER}_${GIT_VER}"
+
+# Create version file
+echo $VER > $BASE_DIR/VERSION
+/bin/chmod +rx $BASE_DIR/VERSION
+
+cd $BASE_DIR
+
+./setup.py bdist_rpm --release="$REL" 
---
+++ setup.py
@@ -0,0 +1,92 @@
+#!/bin/env python3
+
+# CORTX-Py-Utils: CORTX Python common library.
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+import os
+import glob
+from fnmatch import fnmatch
+from setuptools import setup
+import json
+import sys
+
+def get_version() ->str:
+    """ returns version string """
+    if os.path.isfile("./VERSION"):
+        with open("VERSION") as v:
+            return v.read().strip() 
+    return "2.0.0"
+
+
+def get_license() -> str:
+    """ Returns license information """
+    with open('LICENSE', 'r') as lf:
+        return lf.read()
+
+def get_description() -> str:
+    """ Returns description """
+    with open('README.md', 'r') as rf:
+        long_description = rf.read()
+
+def get_install_requirements() -> list:
+    """ Returns pre-requisite """
+
+    with open('requirements.txt') as req:
+        install_requires = [line.strip() for line in req]
+    try:
+        with open('requirements.ext.txt') as req:
+            install_requires = install_requires + [line.strip() for line in req]
+    except Exception:
+        pass
+    return install_requires
+
+def get_conf_files() -> list:
+    """ returns list of conf files """
+
+    return []
+
+def get_requirements_files() -> list:
+    """ Returns requirements file """
+
+    if os.path.exists('requirements.txt'): 
+      return ['requirements.txt']
+    return []
+
+setup(name='cortx-provisioner',
+      version=get_version(),
+      url='https://github.com/Seagate/cortx-provisioner',
+      license='Seagate',
+      author='CORTX Dev',
+      author_email='cortx@seagate.com',
+      description='CORTX Provisioner',
+      package_dir={'provisioner': 'src'},
+      packages=['provisioner'],
+      package_data={
+          'cortx': ['py.typed']
+      },
+      entry_points={
+          'console_scripts': [
+              'cortx_setup = provisioner.cortx_setup:main'
+          ]
+      },
+      data_files=[
+          ('/opt/seagate/cortx/provisioner/conf', get_conf_files()),
+          ('/opt/seagate/cortx/provisioner/conf', get_requirements_files())
+      ],
+      long_description=get_description(),
+      zip_safe=False,
+      python_requires='>=3.6',
+      install_requires=get_install_requirements()
+)
