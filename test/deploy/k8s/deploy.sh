d601ed3002b600b5f2db2613045b37002de15c41
Tanuja Shinde <tanuja.shinde@seagate.com>
2021-09-16 08:27:15 +0530

    EOS-24432: Pushed deploy and destroy script changes for cortx-provisioner dpeloyment POD
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- test/deploy/k8s/deploy.sh
+++ test/deploy/k8s/deploy.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(dirname $0)
 
-./"$SCRIPT_DIR"/destroy.sh
+"$SCRIPT_DIR"/destroy.sh
 
 kubectl create namespace cortx
 kubectl create cm solution-config --from-file="$SCRIPT_DIR"/cluster.yaml \
@@ -17,5 +17,7 @@ echo "Waiting for the containers to start up..."
 sleep 5
 
 kubectl get pods --namespace cortx
-kubectl describe pod cortx-data --namespace cortx
-kubectl logs cortx-data --container cortx-provisioner --namespace cortx
+kubectl describe deployment cortx-provisioner --namespace cortx
+kubectl logs deployment cortx-provisioner --namespace cortx
+#kubectl describe pod cortx-data --namespace cortx
+#kubectl logs cortx-data --container cortx-provisioner --namespace cortx
