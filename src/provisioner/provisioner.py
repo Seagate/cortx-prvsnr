c574be8f6ee82b95fc415593bdb0a001e0820444
Selvakumar <selva.k.nambi@seagate.com>
2021-09-16 17:57:46 +0530

    Change mock pattern
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>

--- src/provisioner/provisioner.py
+++ src/provisioner/provisioner.py
@@ -155,11 +155,18 @@ class CortxProvisioner:
                 node_id)
 
         components = cortx_config_store.get(f'node>{node_id}>components')
-        for comp_name in components.keys():
-            services = cortx_config_store.get(f'node>{node_id}>components>{comp_name}>services')
-            service = 'all' if services is None else ','.join(services)
-            print(f"{comp_name}_setup --config {CortxProvisioner._cortx_conf_url} --services %s" % service)
-
-            # TODO: Enable this code
-            # rc, output = SimpleProcess(f"{components[i]}_setup --config \
-            #  {CortxProvisioner._cortx_conf_url} --services %s" %services.join(","))
+        num_components = len(components)
+        mp_interfaces = ['post_install', 'prepare', 'config', 'init']
+        for interface in mp_interfaces:
+            for j in range(0, num_components):
+                services = cortx_config_store.get(
+                    f'node>{node_id}>components[{j}]>services')
+                service = 'all' if services is None else ','.join(services)
+                cmd_proc = SimpleProcess(f"{components[j]['name']}_setup " \
+                    f"{interface} --config {cortx_conf_url} --services %s" \
+                    % service)
+                _, err, rc = cmd_proc.run()
+                if rc != 0:
+                    raise CortxProvisionerError(rc, "Unable to execute " \
+                        "%s phase for %s. %s", interface, components[j]['name'], \
+                        err)
