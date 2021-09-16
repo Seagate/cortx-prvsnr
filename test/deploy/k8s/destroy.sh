a86ea727dcaaf3b7ee96ebb44ae8fdcee21295bc
Kailash Yadav <kailash.yadav@seagate.com>
2021-09-16 14:16:25 +0530

    EOS-24915:Added kubernets secret deployment files (#6152)
    Signed-off-by: Kailash Yadav <kailash.yadav@seagate.com>

--- test/deploy/k8s/destroy.sh
+++ test/deploy/k8s/destroy.sh
@@ -5,3 +5,4 @@ kubectl delete pvc cortx-config-pvc --namespace cortx
 kubectl delete pv cortx-config-pv --namespace cortx
 kubectl delete cm solution-config --namespace cortx
 kubectl delete cm node-id --namespace cortx
+kubectl delete secret cortx-secret --namespace cortx
