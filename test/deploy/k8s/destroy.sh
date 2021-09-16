dede961780d55d43fde2f29398a0919f832cd91e
Tanuja Shinde <tanuja.shinde@seagate.com>
2021-09-16 12:46:24 +0530

    updated destroy script
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- test/deploy/k8s/destroy.sh
+++ test/deploy/k8s/destroy.sh
@@ -4,5 +4,4 @@ kubectl delete deployment cortx-provisioner --namespace cortx
 kubectl delete pvc cortx-config-pvc --namespace cortx
 kubectl delete pv cortx-config-pv --namespace cortx
 kubectl delete cm solution-config --namespace cortx
-kubectl delete cm node-id --namespace cortx
 kubectl delete secret cortx-secret --namespace cortx
