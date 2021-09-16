70d061cc17cf41a0f793483136f7e2e609a69d87
Selvakumar <selva.k.nambi@seagate.com>
2021-09-16 16:52:17 +0530

    Remove node_id parameter
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>

--- test/setup/test_provisioner.py
+++ test/setup/test_provisioner.py
@@ -44,7 +44,7 @@ class TestProvisioner(unittest.TestCase):
 
         rc = 0
         try:
-            CortxProvisioner.cluster_bootstrap('1', cortx_conf_url)
+            CortxProvisioner.cluster_bootstrap(cortx_conf_url)
 
         except Exception as e:
             print('Exception: ', e)
