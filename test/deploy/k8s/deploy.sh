7b17e3296040a2b9d9132706735c7ec60215cf59
Tanuja Shinde <tanuja.shinde@seagate.com>
2021-09-16 15:43:38 +0530

    EOS-24770: Add machne-id to each POD in kubernetes cluster
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- test/deploy/k8s/deploy.sh
+++ test/deploy/k8s/deploy.sh
@@ -4,14 +4,15 @@ SCRIPT_DIR=$(dirname $0)
 kubectl create namespace cortx
 kubectl create cm solution-config --from-file="$SCRIPT_DIR"/cluster.yaml \
     --from-file="$SCRIPT_DIR"/config.yaml --namespace cortx
-#kubectl apply -f "$SCRIPT_DIR"/cortx-cm-machine-id.yml --namespace cortx
+
 kubectl apply -f "$SCRIPT_DIR"/cortx-secret.yml --namespace cortx
 kubectl apply -f "$SCRIPT_DIR"/cortx-pv-config.yml --namespace cortx
 kubectl apply -f "$SCRIPT_DIR"/cortx-pvc-config.yml --namespace cortx
-kubectl apply -f "$SCRIPT_DIR"/cortx-deployment.yml --namespace cortx
+kubectl apply -f "$SCRIPT_DIR"/cortx-statefulset.yml --namespace cortx
 echo "Waiting for the containers to start up..."
 sleep 5
-kubectl describe deployment cortx-provisioner --namespace cortx
+
+kubectl get statefulset podnode --namespace cortx
 kubectl get pods --namespace cortx
-pod=$(kubectl get pods --namespace cortx | grep cortx | awk '{print $1;}')
+pod=$(kubectl get pods --namespace cortx | grep podnode | awk '{print $1;}')
 kubectl logs $pod --namespace cortx
