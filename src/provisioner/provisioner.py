f01ca72b8478a025144c380ebf127a33f7d33d60
Kailash Yadav <kailash.yadav@seagate.com>
2021-09-17 04:25:34 +0530

    Added Encryption of Secret Key Source Code
    Signed-off-by: Kailash Yadav <kailash.yadav@seagate.com>

--- src/provisioner/provisioner.py
+++ src/provisioner/provisioner.py
@@ -51,27 +51,36 @@ class CortxProvisioner:
             cortx_conf_url = CortxProvisioner._cortx_conf_url
         cortx_config_store = ConfigStore(cortx_conf_url)
         # source code for encrypting and storing secret key
-        cluster_id = Conf.get(CortxProvisioner._solution_index, 'cluster>id')
-        if cluster_id is None:
-            raise CortxProvisionerError(errno.EINVAL, 'Cluster ID not specified')
-        encryption_key = Cipher.gen_key(cluster_id, 'cluster')
-        for key in Conf.get_keys(CortxProvisioner._solution_index):
-            # TODO: use /etc/cortx/solution/secret to confirm secret 
-            if key.endswith('secret'):
-                secret_val = Conf.get(CortxProvisioner._solution_index, key)
-                val = None
-                with open(os.path.join('/etc/cortx/solution/secret', secret_val)) as f:
-                    val = f.read()
-                if val is None:
-                    raise CortxProvisionerError(errno.EINVAL,
-                        'Could not find the Secret in /etc/cortx/solution/secret')
-                val = Cipher.encrypt(encryption_key, val.encode('ascii'))
-                Conf.set(CortxProvisioner._solution_index, key, val)
+        encryption_key = None
 
         if Conf.get(CortxProvisioner._solution_index, 'cluster') is not None:
+            # generating encryption key
+            cluster_id = Conf.get(CortxProvisioner._solution_index, 'cluster>id')
+            if cluster_id is None:
+                raise CortxProvisionerError(errno.EINVAL, 'Cluster ID not specified')
+            # TODO: Find a better way to store the encryption key
+            encryption_key = Cipher.gen_key(cluster_id, 'cluster')
+            with open('/etc/encrypt', 'wb') as en:
+                en.write(encryption_key)
+            
             CortxProvisioner.config_apply_cluster(cortx_config_store)
 
         if Conf.get(CortxProvisioner._solution_index, 'cortx') is not None:
+            # TODO: Loop through all the secret keys
+            # TODO: use /etc/cortx/solution/secret to confirm secret 
+            key = 'cortx>external>kafka>secret'
+            secret_val = Conf.get(CortxProvisioner._solution_index, key)
+            val = None
+            with open(os.path.join('/etc/cortx/solution/secret', secret_val)) as f:
+                val = f.read()
+            if val is None:
+                raise CortxProvisionerError(errno.EINVAL,
+                    'Could not find the Secret in /etc/cortx/solution/secret')
+            with open('/etc/encrypt', 'rb') as en:
+                encryption_key = en.read()
+            val = Cipher.encrypt(encryption_key, val.encode('ascii'))
+            Conf.set(CortxProvisioner._solution_index, key, val)
+
             CortxProvisioner.config_apply_cortx(cortx_config_store)
 
 
