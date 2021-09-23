2bdff743b431948bb01ecc81485317ed5a76b028
Tanuja Shinde <tanuja.shinde@seagate.com>
2021-09-23 08:57:27 +0530

    EOS-24455: updated controlnode and storagenode logic in provisioner kubernetes deployment
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- test/deploy/k8s/deploy.sh
+++ test/deploy/k8s/deploy.sh
@@ -6,13 +6,14 @@ kubectl create namespace cortx
 kubectl create cm solution-config --from-file="$SCRIPT_DIR"/cluster.yaml \
     --from-file="$SCRIPT_DIR"/config.yaml --namespace cortx
 
+kubectl apply -f "$SCRIPT_DIR"/cortx-controlnode-service.yml --namespace cortx\
 kubectl apply -f "$SCRIPT_DIR"/cortx-storagenode-service.yml --namespace cortx
-kubectl apply -f "$SCRIPT_DIR"/cortx-controlnode-service.yml --namespace cortx
 kubectl apply -f "$SCRIPT_DIR"/cortx-secret.yml --namespace cortx
 kubectl apply -f "$SCRIPT_DIR"/cortx-pv-config.yml --namespace cortx
 kubectl apply -f "$SCRIPT_DIR"/cortx-pvc-config.yml --namespace cortx
-kubectl apply -f "$SCRIPT_DIR"/cortx-storagenode-statefulset.yml --namespace cortx
 kubectl apply -f "$SCRIPT_DIR"/cortx-controlnode-statefulset.yml --namespace cortx
+kubectl apply -f "$SCRIPT_DIR"/cortx-storagenode-statefulset.yml --namespace cortx
+
 echo "Waiting for the containers to start up..."
 sleep 10
 
