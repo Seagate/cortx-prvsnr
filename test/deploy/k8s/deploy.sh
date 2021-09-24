e685918fcb03a86b4af4faeb0f95b38bd5f37211
Tanuja Shinde <tanuja.shinde@seagate.com>
2021-09-24 17:35:46 +0530

    EOS-24455: updated controlnode and storagenode logic in provisioner kubernetes deployment (#6176)
    * EOS-24455: updated controlnode and storagenode logic in provisioner kubernetes deployment
    
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>
    
    * EOS-24455: updated controlnode and storagenode logic in provisioner kubernetes deployment
    
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>
    
    * EOS-24455: updated controlnode and storagenode logic in provisioner kubernetes deployment
    
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>
    
    * EOS-24455: updated controlnode and storagenode logic in provisioner kubernetes deployment
    
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>
    
    * EOS-24455: updated controlnode and storagenode logic in provisioner kubernetes deployment
    
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>
    
    * EOS-24455: updated controlnode and storagenode logic in provisioner kubernetes deployment
    
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- test/deploy/k8s/deploy.sh
+++ test/deploy/k8s/deploy.sh
@@ -1,21 +1,26 @@
 #!/bin/bash -x
 set -eu
 SCRIPT_DIR=$(dirname $0)
-"$SCRIPT_DIR"/destroy.sh
+"$SCRIPT_DIR"/destroy.sh || true
 kubectl create namespace cortx
 kubectl create cm solution-config --from-file="$SCRIPT_DIR"/cluster.yaml \
     --from-file="$SCRIPT_DIR"/config.yaml --namespace cortx
 
+kubectl apply -f "$SCRIPT_DIR"/cortx-controlnode-service.yml --namespace cortx\
+kubectl apply -f "$SCRIPT_DIR"/cortx-storagenode-service.yml --namespace cortx
 kubectl apply -f "$SCRIPT_DIR"/cortx-secret.yml --namespace cortx
 kubectl apply -f "$SCRIPT_DIR"/cortx-pv-config.yml --namespace cortx
 kubectl apply -f "$SCRIPT_DIR"/cortx-pvc-config.yml --namespace cortx
-kubectl apply -f "$SCRIPT_DIR"/cortx-statefulset.yml --namespace cortx
+kubectl apply -f "$SCRIPT_DIR"/cortx-controlnode-statefulset.yml --namespace cortx
+kubectl apply -f "$SCRIPT_DIR"/cortx-storagenode-statefulset.yml --namespace cortx
+
 echo "Waiting for the containers to start up..."
 sleep 10
 
-kubectl get statefulset podnode --namespace cortx
+kubectl get statefulset controlnode --namespace cortx
+kubectl get statefulset storagenode --namespace cortx
 kubectl get pods --namespace cortx
-pods=$(kubectl get pods --namespace cortx | grep podnode | awk '{print $1;}')
+pods=$(kubectl get pods --namespace cortx | grep -e "control" -e "storage" | awk '{print $1;}')
 for pod in $pods
 do
     echo "---- $pod ----"
