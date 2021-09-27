57715e10e3e0ef4a11129450280974c53b2d47f7
Tanuja Shinde <tanuja.shinde@seagate.com>
2021-09-27 12:34:32 +0530

    EOS-24455: updated folder structure as well as addressed comments for deploy and destroy scripts
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- test/deploy/kubernetes/deploy.sh
+++ test/deploy/kubernetes/deploy.sh
@@ -32,8 +32,8 @@ kubectl apply -f "$BASEPATH"/solution-config/secrets.yaml --namespace $NAMESPACE
 # Create Persistent Volumes for Block Devices (PV)
 for NODE_INDEX in $(seq 1 $MAXNODES); do
     NODE_NAME="node$NODE_INDEX";
-    print_header "Creating Persistent Volumes for Block Devices - "$NODE_NAME""
-    NODE_BDEV="$BASEPATH"/persistent-volumes/block_devices_"$NODE_NAME".yaml;
+    print_header "Creating Persistent Volumes for Block Devices - $NODE_NAME"
+    NODE_BDEV="$BASEPATH/persistent-volumes/block_devices_$NODE_NAME.yaml";
     kubectl apply -f "$NODE_BDEV" --namespace "$NAMESPACE";
     sleep 2;
     kubectl get pv --namespace "$NAMESPACE" | grep "$NODE_NAME";
@@ -42,8 +42,8 @@ done
 # Create Persistent Volume Claims for Block Devices (PVC)
 for NODE_INDEX in $(seq 1 $MAXNODES); do
     NODE_NAME="node$NODE_INDEX";
-    print_header "Creating Persistent Volume Claims for Block Devices - "$NODE_NAME""
-    NODE_BVOL="$BASEPATH"/volume-claims/block_volumes_"$NODE_NAME".yaml;
+    print_header "Creating Persistent Volume Claims for Block Devices - $NODE_NAME"
+    NODE_BVOL="$BASEPATH/volume-claims/block_volumes_$NODE_NAME.yaml";
     kubectl apply -f "$NODE_BVOL" --namespace "$NAMESPACE";
     sleep 2;
 done
@@ -51,8 +51,8 @@ done
 # Create Persistent Volumes for Storage Devices (PV)
 for NODE_INDEX in $(seq 1 $MAXNODES); do
     NODE_NAME="node$NODE_INDEX";
-    print_header "Creating Persistent Volumes for Storage Devices - "$NODE_NAME""
-    NODE_SDEV="$BASEPATH"/persistent-volumes/storage_devices_"$NODE_NAME".yaml;
+    print_header "Creating Persistent Volumes for Storage Devices - $NODE_NAME"
+    NODE_SDEV="$BASEPATH/persistent-volumes/storage_devices_$NODE_NAME.yaml";
     kubectl apply -f "$NODE_SDEV" --namespace "$NAMESPACE";
     sleep 2;
     kubectl get pv --namespace "$NAMESPACE" | grep "$NODE_NAME";
@@ -61,25 +61,25 @@ done
 # Create Persistent Volume Claims for Storage Devices (PVC)
 for NODE_INDEX in $(seq 1 $MAXNODES); do
     NODE_NAME="node$NODE_INDEX";
-    print_header "Creating Persistent Volume Claims for Storage Devices - "$NODE_NAME""
-    NODE_SVOL="$BASEPATH"/volume-claims/storage_volumes_"$NODE_NAME".yaml;
+    print_header "Creating Persistent Volume Claims for Storage Devices - $NODE_NAME"
+    NODE_SVOL="$BASEPATH/volume-claims/storage_volumes_$NODE_NAME.yaml";
     kubectl apply -f "$NODE_SVOL" --namespace "$NAMESPACE";
     sleep 2;
 done
 
 # Create Control Service (Headless)
 print_header "Creating Cotrol Service - Cluster"
-kubectl apply -f "$BASEPATH"/external-services/headless_control_node.yaml --namespace $NAMESPACE
+kubectl apply -f "$BASEPATH/external-services/headless_control_node.yaml" --namespace "$NAMESPACE"
 
 # Create Control Node (POD)
 print_header "Creating Cotrol Node - Cluster"
-kubectl apply -f "$BASEPATH"/provisioner-pods/deployment_control_node.yaml --namespace $NAMESPACE
+kubectl apply -f "$BASEPATH/provisioner-pods/deployment_control_node.yaml" --namespace "$NAMESPACE"
 
 # Create Storage Service (Headless)
 for NODE_INDEX in $(seq 1 $MAXNODES); do
     NODE_NAME="node$NODE_INDEX";
-    print_header "Creating Storage Service - "$NODE_NAME""
-    NODE_SVC="$BASEPATH"/external-services/headless_storage_"$NODE_NAME".yaml;
+    print_header "Creating Storage Service - $NODE_NAME"
+    NODE_SVC="$BASEPATH/external-services/headless_storage_$NODE_NAME.yaml";
     kubectl apply -f "$NODE_SVC" --namespace "$NAMESPACE";
     sleep 2;
 done
@@ -87,8 +87,8 @@ done
 # Create Storage Node (POD)
 for NODE_INDEX in $(seq 1 $MAXNODES); do
     NODE_NAME="node$NODE_INDEX";
-    print_header "Creating Storage Node - "$NODE_NAME""
-    NODE_POD="$BASEPATH"/provisioner-pods/deployment_storage_"$NODE_NAME".yaml;
+    print_header "Creating Storage Node - $NODE_NAME"
+    NODE_POD="$BASEPATH/provisioner-pods/deployment_storage_$NODE_NAME.yaml";
     kubectl apply -f "$NODE_POD" --namespace "$NAMESPACE";
     sleep 2;
 done
