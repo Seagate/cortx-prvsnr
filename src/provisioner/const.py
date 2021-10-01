d37a93363ffb9f9bc9475855813dea2aad287694
MARIYAPPAN PONNUSAMY <mariyappan.ponnusamy@seagate.com>
2021-10-01 00:29:07 -0600

    Change copyright year to 2021 in const.py
    Signed-off-by: MARIYAPPAN PONNUSAMY <mariyappan.ponnusamy@seagate.com>

--- src/provisioner/const.py
+++ src/provisioner/const.py
@@ -1,5 +1,5 @@
 # CORTX Python common library.
-# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
