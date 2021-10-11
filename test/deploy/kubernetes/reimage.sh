8d5b297b7017009d1394ffed46cb5bcb856a6b4f
Tanuja Shinde <tanuja.shinde@seagate.com>
2021-10-11 11:30:30 +0530

    Updated config,yaml fir s3 endpoints
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- test/deploy/kubernetes/reimage.sh
+++ test/deploy/kubernetes/reimage.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Defaults
 IMAGE_NAME="ghcr.io/seagate/cortx-all"
-IMAGE_TAG="2.0.0-latest-custom-ci"
+LATEST_TAG="2.0.0-latest-custom-ci"
+CUSTOM_TAG="2.0.0-266-RC2"
 IMAGE_LIST="/tmp/cortx_images.log"
 LOCAL_PATH="/etc/cortx"
 SHARE_PATH="/share"
@@ -15,14 +16,16 @@ sysctl -w vm.max_map_count=30000000;
 
 # Delete previous images
 docker images | grep "cortx-all" | tr -s ' ' | cut -d ' ' -f 3 > $IMAGE_LIST;
-for IMAGE_MATCH in $(cat $IMAGE_LIST); do
+for IMAGE_MATCH in `cat $IMAGE_LIST`; do
     echo -e "Deleting Image ID: $IMAGE_MATCH";
     docker rmi -f $IMAGE_MATCH;
 done
 
 # Download latest image
-echo -e "Pulling Latest Image: $IMAGE_NAME:$IMAGE_TAG";
-docker pull $IMAGE_NAME:$IMAGE_TAG;
+echo -e "Pulling Custom Image: $IMAGE_NAME:$CUSTOM_TAG";
+docker pull $IMAGE_NAME:$CUSTOM_TAG;
+echo -e "Renaming Custom Image: $IMAGE_NAME:$CUSTOM_TAG to Latest Image: $IMAGE_NAME:$LATEST_TAG";
+docker tag $IMAGE_NAME:$CUSTOM_TAG $IMAGE_NAME:$LATEST_TAG;
 
 # Delete local
 echo -e "Cleaning Local: $LOCAL_PATH";
