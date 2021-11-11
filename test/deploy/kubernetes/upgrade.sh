cc6ff83e52cfebc0efc5980f3a54be37245c0cb3
Tanuja Shinde <tanuja.shinde@seagate.com>
2021-11-11 10:13:08 +0530

    EOS-25797: Updated sleep in upgrade script
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- test/deploy/kubernetes/upgrade.sh
+++ test/deploy/kubernetes/upgrade.sh
@@ -2,7 +2,7 @@
 BASEPATH=$(dirname $0)
 MAXNODES=$(kubectl get nodes | awk -v col=1 '{print $col}' | tail -n+2 | wc -l)
 NAMESPACE="default"
-TIMEDELAY="20"
+TIMEDELAY="10"
 
 function show_usage {
     echo -e "usage: $(basename $0) [-i UPGRADE-IMAGE]"
@@ -51,25 +51,21 @@ for NODE_INDEX in $(seq 1 $MAXNODES); do
     NODE_NAME="node$NODE_INDEX";
     print_header "Pausing Runtime Storage Node $STORAGE_NODE";
     kubectl rollout pause deployment $STORAGE_NODE;
-    sleep $TIMEDELAY;
 
     # Create Upgrade Storage POD
     print_header "Creating Upgrade Storage Node - $NODE_NAME";
     NODE_POD="$BASEPATH/upgrade-pods/upgrade_storage_$NODE_NAME.yaml";
     kubectl apply -f "$NODE_POD" --namespace "$NAMESPACE";
-    sleep $TIMEDELAY;
 
     # Wait for Upgrade Storage-POD execution
     printf "\nWaiting for Upgrade Storage Node $NODE_NAME";
     while [ "$(kubectl get pods | grep -E "(^|\s)$STORAGE_NODE($|\s)" | awk '{print $3}')" != "Completed" ]; do
         printf ".";
-        sleep $TIMEDELAY;
     done
 
     # Upgrade Runtime POD for each node
     print_header "Upgrading Runtime Storage Node for - $NODE_NAME";
     kubectl set image deployment $STORAGE_NODE cortx-hax=$UPGRADE_IMAGE cortx-confd=$UPGRADE_IMAGE cortx-ios1=$UPGRADE_IMAGE cortx-ios2=$UPGRADE_IMAGE cortx-s31=$UPGRADE_IMAGE  cortx-s32=$UPGRADE_IMAGE cortx-s3auth=$UPGRADE_IMAGE  cortx-s3haproxy=$UPGRADE_IMAGE cortx-s3bgc=$UPGRADE_IMAGE;
-    sleep $TIMEDELAY;
 
     # Resume Runtime Storage
     print_header "Resuming Runtime Storage Node - Cluster";
@@ -90,13 +86,11 @@ kubectl apply -f "$BASEPATH/upgrade-pods/upgrade_control_node.yaml" --namespace 
 printf "\nWaiting for Upgrade Control Node.";
 while [ "$(kubectl get pods | grep -E "(^|\s)control-node($|\s)" | awk '{print $3}')" != "Completed" ]; do
     printf ".";
-    sleep $TIMEDELAY;
 done
 printf "\n";
 
 # Upgrade Runtime Control POD
 kubectl set image deployment control-node csm-agent=$UPGRADE_IMAGE s3-bg-producer=$UPGRADE_IMAGE
-sleep $TIMEDELAY;
 
 # Resume Runtime Control POD
 print_header "Resuming Runtime Control Node - Cluster";
