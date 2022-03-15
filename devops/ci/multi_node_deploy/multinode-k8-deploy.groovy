acf72fcd87f3f93335202deec99b26e9ba35f446
Aayushi Sharma <aayushi.sharma@seagate.com>
2022-03-15 04:43:38 -0600

    Modified groovy
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>

--- devops/ci/multi_node_deploy/multinode-k8-deploy.groovy
+++ devops/ci/multi_node_deploy/multinode-k8-deploy.groovy
@@ -1,7 +1,5 @@
 def remote_host() {
-    echo "Inside Func"
-    echo "${hosts}"
-    hosts = "${hosts}"
+    hosts = "${NODE_HOST_LIST}"
     remotes = []
     hosts = hosts.split("\n")
     for (int num = 0; num < hosts.size(); num++) {
@@ -23,8 +21,6 @@ pipeline {
         }
     }
 
-    // triggers { cron('30 19 * * *') }
-
     options {
         timeout(time: 240, unit: 'MINUTES')
         timestamps()
@@ -33,89 +29,98 @@ pipeline {
 
     environment {
         WORK_SPACE = "/root/cortx-k8s/k8_cortx_cloud"
-        // NODES_HOST = "$(cat ${hosts} | awk -F[,] '{print $1}' | cut -d'=' -f2)"
-        // PRIMARY_NODE = "$(head -1 ${hosts} | awk -F[,] '{print $1}' | cut -d'=' -f2)"
-        // WORKER_NODES = "$(cat ${hosts} | grep -v ${PRIMARY_NODE} | awk -F[,] '{print $1}' | cut -d'=' -f2)"
     }
 
     parameters {
         string(name: 'CORTX_SCRIPTS_REPO', defaultValue: 'https://github.com/Seagate/cortx-k8s', description: 'Repository for cortx-k8s scripts (Services Team)', trim: true)
         string(name: 'CORTX_SCRIPTS_BRANCH', defaultValue: 'cortx-test', description: 'cortx-k8s scripts (Provisioner Team)', trim: true)
-        // string(name: 'SCRIPTS_REPO', defaultValue: 'https://github.com/ashukakkar/cortx-prvsnr-1', description: 'Repository for Cluster Setup scripts', trim: true)
-        text(defaultValue: '''hostname=<hostname>,user=<user>,pass=<password>''', description: 'VM details to be used. First node will be used as Master', name: 'hosts')
+        text(defaultValue: '''hostname=<hostname>,user=<user>,pass=<password>''', description: 'VM details to be used. First node will be used as Master', name: 'NODE_HOST_LIST')
+        booleanParam(name: 'SETUP_K8s_CLUSTER', defaultValue: false, description: 'Selecting this option will setup K8s Cluster before running Deployment.')
     }
 
     stages {
 
-        // stage('Checkout Script') {
-        //     steps {
-        //         script {
-        //             echo "Inside stage"
-        //             remotes = remote_host()
-        //             echo "${hosts}"
-        //             echo "${remotes}"
-        //             for (remote in remotes) {
-        //                 echo "$remote"
-        //                 sshCommand remote: remote, command: """
-        //                     cd /root
-        //                     git clone ${CORTX_SCRIPTS_REPO} -b ${CORTX_SCRIPTS_BRANCH}
-        //                 """
-        //             }
-        //         }
-        //     }
-        // }
+        stage('Setup K8s Cluster'){
+            when { expression { params.SETUP_K8s_CLUSTER } }
+            steps {
+                script {
+                    catchError(stageResult: 'FAILURE') {
+                        build job: '/Cortx-kubernetes/setup-kubernetes-cluster', wait: true,
+                        parameters: [
+                            string(name: 'hosts', value: "${NODE_HOST_LIST}"),
+                            booleanParam(name: 'PODS_ON_PRIMARY', value: "${SETUP_K8s_CLUSTER}")
+                        ]
+                    }
+                }
+            }
+        }
+
+        stage('Checkout CORTX Script') {
+            steps {
+                script {
+                    remotes = remote_host()
+                    for (remote in remotes) {
+                        sshCommand remote: remote, command: """
+                            cd /root
+                            rm -rf cortx-k8s
+                            git clone ${CORTX_SCRIPTS_REPO} -b ${CORTX_SCRIPTS_BRANCH}
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Remove solution.yaml') {
+            steps {
+                script {
+                    for (remote in remotes) {
+                        sshCommand remote: remote, command: """
+                            rm -rf ${WORK_SPACE}/solution.yaml
+                        """
+                    }
+                }
+            }
+        }
 
         stage('Update solution.yaml') {
             steps {
+                sh label: 'Update solution.yaml', script: '''
+                    pushd ${WORKSPACE}/devops/ci
+                        echo $NODE_HOST_LIST | tr ' ' '\n' > hosts
+                        cat hosts
+                        export CONTROL_IMAGE=${CONTROL_IMAGE}
+                        export DATA_IMAGE=${DATA_IMAGE}
+                        export HA_IMAGE=${HA_IMAGE}
+                        export SERVER_IMAGE=${SERVER_IMAGE}
+                        sh update_solution.sh
+                    popd
+                ''' 
+            }
+        }
+
+        stage('Run prerequisite script') {
+            steps {
                 script {
-                    sh label: '', script: "find / -name 'solution.yaml'"
-                    sh label: '', script: "ls ${WORKSPACE}"
-                    sh label: '', script: "ls /var/jenkins/workspace/"
-                    cat input/solution.yaml
-                    echo "$input/solution.yaml"
-                    // remotes = remote_host()
-                    // for (remote in remotes) {
-                    //     echo "$remote"
-                    //     sshCommand remote: remote, command: """
-                    //         rm -f ${WORK_SPACE}/solution.yaml
-                    //         cp input/solution.yaml ${WORK_SPACE}/solution.yaml
-                    //     """
-                    // }
+                    for (remote in remotes) {
+                        sshCommand remote: remote, command: """
+                            cd ${WORK_SPACE}
+                            ./prereq-deploy-cortx-cloud.sh /dev/sdb
+                        """
+                    }
                 } 
             }
         }
 
-    //     // stage('Copy prereq and solution.yaml') {
-    //     //     for (node in $WORKER_NODES){
-    //     //         sshCommand remote: remotes[0], command: """
-    //     //             scp -q $WORK_SPACE/solution.yaml "$node":./
-    //     //             scp -q $WORK_SPACE/prereq-deploy-cortx-cloud.sh "$node":./
-    //     //         """
-    //     //     }
-    //     // }
-
-    //     stage('Run prerequisite script') {
-    //         steps {
-    //             script {
-    //                 remotes = remote_host(${hosts})
-    //                 for (remote in remotes) {
-    //                     sshCommand remote: remote, command: """
-    //                     sh './${WORK_SPACE}/prereq-deploy-cortx-cloud.sh /dev/sdb' 
-    //                 """
-    //                 }
-    //             } 
-    //         }
-    //     }
-
-    //     stage('Deploy CORTX Cluster') {
-    //         steps {
-    //             script {
-    //                 sshCommand remote: remotes[0], command: """
-    //                 sh './${WORK_SPACE}/deploy-cortx-cloud.sh'
-    //             """
-    //             }
-    //         }
-    //     }
+        stage('Deploy CORTX Cluster') {
+            steps {
+                script {
+                    sshCommand remote: remotes[0], command: """
+                        cd ${WORK_SPACE}
+                        sh deploy-cortx-cloud.sh
+                    """
+                }
+            }
+        }
     }
 
     post {
@@ -128,24 +133,17 @@ pipeline {
             }
         }
 
-        // failure {
-        //     script {}
-        // }
-
-        // always {
-        //     script {
-        //         def recipientProvidersClass = [[$class: 'RequesterRecipientProvider']]
-        //         // mailRecipients = "shailesh.vaidya@seagate.com"
-        //         emailext ( 
-        //             // body: '''${SCRIPT, template="cluster-setup-email.template"}''',
-        //             // mimeType: 'text/html',
-        //             subject: "[Jenkins Build ${currentBuild.currentResult}] : ${env.JOB_NAME}",
-        //             attachLog: true,
-        //             to: "${mailRecipients}",
-        //             recipientProviders: recipientProvidersClass
-        //         )
-        //         cleanWs()
-        //     }
-        // }
+        always {
+            script {
+                def recipientProvidersClass = [[$class: 'RequesterRecipientProvider']]
+                emailext ( 
+                    body: '''${SCRIPT, template="cluster-setup-email.template"}''',
+                    mimeType: 'text/html',
+                    subject: "[Jenkins Build ${currentBuild.currentResult}] : ${env.JOB_NAME}",
+                    attachLog: true,
+                    recipientProviders: recipientProvidersClass
+                )
+            }
+        }
     }
 }
