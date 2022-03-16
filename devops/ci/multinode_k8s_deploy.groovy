48762a257f7512f1db0d0933c67d6ae706eb1914
Aayushi Sharma <aayushi.sharma@seagate.com>
2022-03-16 05:16:40 -0600

    Added Prereq
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>

--- devops/ci/multinode_k8s_deploy.groovy
+++ devops/ci/multinode_k8s_deploy.groovy
@@ -123,7 +123,7 @@ pipeline {
                     for (remote in remotes) {
                         sshCommand remote: remote, command: """
                             cd ${WORK_SPACE}
-                            ./prereq-deploy-cortx-cloud.sh /dev/sdb
+                            ./prereq-deploy-cortx-cloud.sh -d /dev/sdb
                         """
                     }
                 } 
