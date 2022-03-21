28e305f1143f54f7b6699dbfb50ba4873a100851
Aayushi Sharma <aayushi.sharma@seagate.com>
2022-03-21 06:17:23 -0600

    Added mail-id
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>

--- devops/ci/multinode_k8s_deploy.groovy
+++ devops/ci/multinode_k8s_deploy.groovy
@@ -177,7 +177,7 @@ pipeline {
         always {
             script {
                 def recipientProvidersClass = [[$class: 'RequesterRecipientProvider']]
-                mailRecipients = "aayushi.sharma@seagate.com,karan.kakkar@seagate.com"
+                mailRecipients = "CORTX.Provisioner@seagate.com"
                 emailext ( 
                     body: '''${SCRIPT, template="cluster-setup-email.template"}''',
                     mimeType: 'text/html',
