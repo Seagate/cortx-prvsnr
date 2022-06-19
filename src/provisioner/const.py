88284d865198485a581e8803583372021b8163e8
Zahid Shaikh <zahid.shaikh@seagate.com>
2022-06-18 21:14:12 -0600

    CORTX-32256 Provisioner Logs for cortx_setup commands are not getting displayed in deployment pod logs redirecting log to console for provisioner
    Signed-off-by: Zahid Shaikh <zahid.shaikh@seagate.com>

--- src/provisioner/const.py
+++ src/provisioner/const.py
@@ -30,3 +30,4 @@ REQUIRED_EXTERNAL_SW = ['kafka', 'consul']
 # Path to the location that holds the cortx gconf consul url.
 CONSUL_CONF_URL = "/etc/cortx/consul_conf"
 CORTX_CHANGESET_URL = "yaml:///etc/cortx/changeset.conf"
+DEFAULT_CONSOLE_OUTPUT_LEVEL = "INFO"
