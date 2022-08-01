da98ff005310f93f6e49b2fd0255900336de233f
Tanuja Shinde <tanuja.shinde@seagate.com>
2022-08-01 09:59:05 +0530

    CORTX-33648: rebase with latest main
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- src/provisioner/provisioner.py
+++ src/provisioner/provisioner.py
@@ -475,7 +475,7 @@ class CortxProvisioner:
         Conf.set(CortxProvisioner._cortx_gconf_consul_index, f'{key_prefix}>phase', phase)
         Conf.set(CortxProvisioner._cortx_gconf_consul_index, f'{key_prefix}>status', status)
         if phase is ProvisionerStages.DEPLOYMENT.value:
-            Conf.set(_conf_idx, f'{key_prefix}>time', int(time.time()))
+            cortx_conf.set(f'{key_prefix}>time', int(time.time()))
             Conf.set(CortxProvisioner._cortx_gconf_consul_index, f'{key_prefix}>time', int(time.time()))
         Conf.save(CortxProvisioner._cortx_gconf_consul_index)
 
