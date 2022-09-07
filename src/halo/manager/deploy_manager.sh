b53eef36161698f4da500cd8016e28de2f86ffce
Tanuja Shinde <tanuja.shinde@seagate.com>
2022-09-07 08:03:27 -0600

    implement manager_setup independant of cortx
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- src/halo/manager/deploy_manager.sh
+++ src/halo/manager/deploy_manager.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+kubectl create namespace halo
 kubectl create secret generic configuration-secrets \
   --from-literal=consul_admin_secret=Seagate@123 \
   --from-literal=kafka_admin_secret=Seagate@123 \
   --from-literal=s3_auth_admin_secret=Seagate@123 \
-  --from-literal=csm_mgmt_admin_secret=Seagate@123
+  --from-literal=csm_mgmt_admin_secret=Seagate@123 -n halo
 
-kubectl create secret generic manager-ssl-cert --from-file=/opt/seagate/halo/manager/s3.seagate.com.pem
+kubectl create secret generic manager-ssl-cert --from-file=/opt/seagate/halo/manager/s3.seagate.com.pem -n halo
 
-kubectl create configmap solution-config --from-file=/opt/seagate/halo/manager/cluster.yaml
-kubectl apply -f /opt/seagate/halo/manager/deployment.yaml
+kubectl create configmap solution-config --from-file=/opt/seagate/halo/manager/cluster.yaml -n halo
+
+# configure consul
+export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/opt/puppetlabs/bin:/usr/local/bin"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+helm repo add hashicorp https://helm.releases.hashicorp.com
+helm install consul hashicorp/consul --values /opt/seagate/halo/manager/config.yaml -n halo
+sleep 50;
+kubectl apply -f /opt/seagate/halo/manager/deployment.yaml -n halo
+sleep 30;
